8c1a40122320053b89338a5345399c1f3e653ef3
Stéphano TELOLAHY <hugues.stephano.telolahy@devoteam.com>
2022-05-30 23:35:38 +0200

    initial commit

--- CardGameDemo/ViewController.swift
+++ CardGameDemo/ViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import CardGameCore
 import CardGameMechanics
 
 class ViewController: UIViewController {
@@ -15,8 +14,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        var cards = Cards.all
-        
+        let cards = Cards.all
+        _ = cards.count
     }
 
 
--- CardGameMechanics/Package.swift
+++ CardGameMechanics/Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "CardGameMechanics",
-            dependencies: []),
+            dependencies: ["CardGameCore"]),
         .testTarget(
             name: "CardGameMechanicsTests",
             dependencies: ["CardGameMechanics"]),
