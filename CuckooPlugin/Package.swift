58797acda221a35ebf9d2994af279fad2af60d52
Stephano Telolahy <>
2023-03-05 16:33:35 +0100

    add Yams

--- CuckooPlugin/Package.swift
+++ CuckooPlugin/Package.swift
@@ -11,7 +11,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Brightify/Cuckoo", from: "1.9.1")
+        .package(url: "https://github.com/Brightify/Cuckoo", from: "1.9.1"),
+        .package(url: "https://github.com/jpsim/Yams.git", from: "5.0.5")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
