6dfb53865b60efc2a11f38f3362c9320f97172c2
Stephano Telolahy <>
2023-03-05 17:13:49 +0100

    update_dependency

--- CardGame/Package.swift
+++ CardGame/Package.swift
@@ -16,8 +16,8 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(path: "../GameDSL"),
-        .package(path: "../CuckooPlugin"),
         .package(url: "https://github.com/Brightify/Cuckoo", from: "1.9.1"),
+        .package(url: "https://github.com/stephtelolahy/CuckooPlugin", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
--- GameDSL/Package.swift
+++ GameDSL/Package.swift
@@ -14,8 +14,8 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(path: "../CuckooPlugin"),
-        .package(url: "https://github.com/Brightify/Cuckoo", from: "1.9.1")
+        .package(url: "https://github.com/Brightify/Cuckoo", from: "1.9.1"),
+        .package(url: "https://github.com/stephtelolahy/CuckooPlugin", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
