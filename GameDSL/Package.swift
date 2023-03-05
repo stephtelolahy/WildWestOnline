755d1c4e44b2ed16bc0d2ae8fced17a9b6c8b6fb
Stephano Telolahy <>
2023-03-05 21:25:51 +0100

    add_swiftlint_plugin

--- GameDSL/Package.swift
+++ GameDSL/Package.swift
@@ -14,21 +14,25 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Brightify/Cuckoo", from: "1.9.1"),
-        .package(url: "https://github.com/stephtelolahy/CuckooPlugin", from: "1.0.0")
+//        .package(url: "https://github.com/Brightify/Cuckoo", from: "1.9.1"),
+//        .package(url: "https://github.com/stephtelolahy/CuckooPlugin", from: "1.0.0"),
+        .package(url: "https://github.com/lukepistrol/SwiftLintPlugin", from: "0.2.2"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GameDSL",
-            dependencies: []),
-        .testTarget(
-            name: "GameDSLTests",
-            dependencies: ["GameDSL", "Cuckoo"],
+            dependencies: [],
             plugins: [
-                .plugin(name: "CuckooPlugin", package: "CuckooPlugin")
-            ]
-        ),
+                .plugin(name: "SwiftLint", package: "SwiftLintPlugin")
+            ]),
+//        .testTarget(
+//            name: "GameDSLTests",
+//            dependencies: ["GameDSL", "Cuckoo"],
+//            plugins: [
+//                .plugin(name: "CuckooPlugin", package: "CuckooPlugin")
+//            ]
+//        ),
     ]
 )
--- GameDSL/Tests/GameDSLTests/GameDSLTests.swift
+++ GameDSL/Tests/GameDSLTests/GameDSLTests.swift
@@ -1,6 +1,6 @@
 import XCTest
-@testable import GameDSL
 import Cuckoo
+@testable import GameDSL
 
 final class GameDSLTests: XCTestCase {
 
