ebc60ab6f6a72d81a04370495291356c7aca6732
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-09-02 17:25:09 +0200

    Cleanup

--- GameKit/Sources/Screen/App/AppState.swift
+++ GameKit/Sources/Screen/App/AppState.swift
@@ -12,16 +12,10 @@ import InitMacro
 @Init
 public struct AppState: Codable, Equatable {
     let screens: [ScreenState]
-
+    
     public init() {
         screens = [.splash]
     }
-}
-
-public enum ScreenState: Codable, Equatable {
-    case splash
-    case home(HomeState)
-    case game(GamePlayState)
 }
 
 public enum AppAction: Action, Codable, Equatable {
@@ -38,47 +32,27 @@ public enum Screen: Codable, Equatable {
 public extension AppState {
     static let reducer: Reducer<Self> = { state, action in
         var screens = state.screens
-
+        
         // Update visible screens
         switch action {
         case AppAction.showScreen(.home):
             screens = [.home(.init())]
-
+            
         case AppAction.dismissScreen(.game):
             screens.removeLast()
-
+            
         case AppAction.showScreen(.game):
             let game = Inventory.createGame(playersCount: 5)
-            screens.append(.game(game.toGamePlayState()))
-
+            let gamePlayState = GamePlayState(players: game.playOrder.map { game.player($0) })
+            screens.append(.game(gamePlayState))
+            
         default:
             break
         }
-
+        
         // Reduce each screen state
         screens = screens.map { ScreenState.reducer($0, action) }
-
+        
         return .init(screens: screens)
-    }
-}
-
-private extension ScreenState {
-    static let reducer: Reducer<Self> = { state, action in
-        switch state {
-        case let .home(homeState):
-            .home(HomeState.reducer(homeState, action))
-
-        case let .game(gameState):
-            .game(GamePlayState.reducer(gameState, action))
-
-        default:
-            state
-        }
-    }
-}
-
-private extension GameState {
-    func toGamePlayState() -> GamePlayState {
-        .init(players: playOrder.map { player($0) })
     }
 }
---
+++ GameKit/Sources/Screen/App/ScreenState.swift
@@ -0,0 +1,28 @@
+//
+//  ScreenState.swift
+//  
+//
+//  Created by Hugues Telolahy on 02/09/2023.
+//
+import Redux
+
+public enum ScreenState: Codable, Equatable {
+    case splash
+    case home(HomeState)
+    case game(GamePlayState)
+}
+
+extension ScreenState {
+    static let reducer: Reducer<Self> = { state, action in
+        switch state {
+        case let .home(homeState):
+            .home(HomeState.reducer(homeState, action))
+
+        case let .game(gameState):
+            .game(GamePlayState.reducer(gameState, action))
+
+        default:
+            state
+        }
+    }
+}
