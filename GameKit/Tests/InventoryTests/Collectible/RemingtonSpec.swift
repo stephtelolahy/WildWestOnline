88a6f5b7ba78524e576fb4615396001eb9cbb831
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-09-05 22:10:52 +0200

    RemingtonSpec

---
+++ GameKit/Tests/InventoryTests/Collectible/RemingtonSpec.swift
@@ -0,0 +1,39 @@
+//
+//  RemingtonSpec.swift
+//
+//
+//  Created by Hugues Telolahy on 18/07/2023.
+//
+
+import Quick
+import Nimble
+import Game
+
+final class RemingtonSpec: QuickSpec {
+    override func spec() {
+        describe("playing remington") {
+            it("should equip") {
+                // Given
+                let state = createGameWithCardRef {
+                    Player("p1") {
+                        Hand {
+                            .remington
+                        }
+                    }
+                    .setupAttribute(.weapon, 1)
+                    .attribute(.weapon, 1)
+                }
+
+                // When
+                let action = GameAction.play(.remington, player: "p1")
+                let result = self.awaitAction(action, state: state)
+
+                // Then
+                expect(result) == [
+                    .playEquipment(.remington, player: "p1"),
+                    .setAttribute(.weapon, value: 3, player: "p1")
+                ]
+            }
+        }
+    }
+}
