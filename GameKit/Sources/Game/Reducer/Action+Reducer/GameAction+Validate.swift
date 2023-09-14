90a0ea8a6747ce5a5bdeb22ed8239ae9eeba0e48
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-09-14 08:31:22 +0200

    Refactor GameAction validate

--- GameKit/Sources/Game/Reducer/Action+Reducer/GameAction+Validate.swift
+++ GameKit/Sources/Game/Reducer/Action+Reducer/GameAction+Validate.swift
@@ -1,27 +1,32 @@
 //
 //  GameAction+Validate.swift
-//  
+//
 //
 //  Created by Hugues Telolahy on 08/07/2023.
 //
 
 extension GameAction {
     func validate(state: GameState) throws {
-        var state = try reduce(state: state)
-        if state.queue.isNotEmpty {
-            let nextAction = state.queue.remove(at: 0)
-            switch nextAction {
-            case let .chooseOne(_, options):
-                for (_, option) in options {
-                    try option.validate(state: state)
-                }
+        switch self {
+        case .activateCards,
+                .setGameOver:
+            return
 
-            case .activateCards:
-                return
-
-            default:
-                try nextAction.validate(state: state)
+        case let .chooseOne(_, options):
+            var state = state
+            state.chooseOne = nil
+            for (_, option) in options {
+                try option.validate(state: state)
             }
+
+        default:
+            var state = try reduce(state: state)
+            guard state.queue.isNotEmpty else {
+                return
+            }
+
+            let next = state.queue.removeFirst()
+            try next.validate(state: state)
         }
     }
 }
@@ -54,7 +59,7 @@ extension GameAction {
         guard !validOptions.isEmpty else {
             throw GameError.noValidOption
         }
-        
+
         return .chooseOne(player: chooser, options: validOptions)
     }
 }
