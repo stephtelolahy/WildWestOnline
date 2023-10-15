de88c5d6f2e6c7be52d2d7af7a8475507d4b35d5
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-10-15 04:23:26 +0200

    stabilize

--- GameKit/Sources/Game/Middleware/GameLoopMiddleware.swift
+++ GameKit/Sources/Game/Middleware/GameLoopMiddleware.swift
@@ -42,7 +42,7 @@ private func evaluateTriggeredEffects(action: GameAction, state: GameState) -> G
     // active players
     for player in state.playOrder {
         let playerObj = state.player(player)
-        let cards = playerObj.inPlay.cards + playerObj.abilities + playerObj.hand.cards
+        let cards = playerObj.inPlay.cards + playerObj.abilities
         for card in cards {
             if let action = triggeredEffect(by: card, player: player, state: state) {
                 triggered.append(action)
