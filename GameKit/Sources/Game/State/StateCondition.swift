1b721d665965a3a24c13305741707c2a812b5f95
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-10-21 10:21:21 +0200

    Cleanup

--- GameKit/Sources/Game/State/StateCondition.swift
+++ GameKit/Sources/Game/State/StateCondition.swift
@@ -14,9 +14,6 @@ public enum StateCondition: Codable, Equatable {
     /// The maximum times per turn a card may be played is X
     case isCardPlayedLessThan(String, ArgNum)
 
-    /// Is actor the current turn
-    case isYourTurn
-
     /// Is not the current actor's turn
     case isOutOfTurn
 }
