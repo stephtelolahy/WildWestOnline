99bbf7b53cb30d288c663035d7bfead2924f9355
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-10-21 11:07:57 +0200

    Refactor

--- GameKit/Sources/Game/Middleware/GameLoopMiddleware.swift
+++ GameKit/Sources/Game/Middleware/GameLoopMiddleware.swift
@@ -11,147 +11,154 @@ public let gameLoopMiddleware: Middleware<GameState> = { state, action in
         return Empty().eraseToAnyPublisher()
     }
 
-    if let gameOver = evaluateGameOver(action: action, state: state) {
-        return Just(gameOver).eraseToAnyPublisher()
-    } else if let effects = evaluateTriggeredEffects(action: action, state: state) {
-        return Just(effects).eraseToAnyPublisher()
-    } else if let nextAction = evaluateNextAction(action: action, state: state) {
-        return Just(nextAction).eraseToAnyPublisher()
-    } else if let activateCards = evaluateActiveCards(state: state) {
-        return Just(activateCards).eraseToAnyPublisher()
-    } else {
-        return Empty().eraseToAnyPublisher()
-    }
-}
+    let middleWares: [GameActionMiddleware] = [
+        GameOverMiddleware(),
+        TriggeredEffectsMiddleware(),
+        NextActionMiddleware(),
+        ActivateCardsMiddleware()
+    ]
 
-// MARK: - Next action
-
-private func evaluateNextAction(action: GameAction, state: GameState) -> GameAction? {
-    switch action {
-    case .setGameOver,
-            .chooseOne,
-            .activateCards:
-        nil
-
-    default:
-        state.queue.first
-    }
-}
-
-// MARK: - Triggered effects
-
-private func evaluateTriggeredEffects(action: GameAction, state: GameState) -> GameAction? {
-    var triggered: [GameAction] = []
-
-    // active players
-    for player in state.playOrder {
-        let playerObj = state.player(player)
-        let cards = triggerableCardsOfActivePlayer(playerObj)
-        for card in cards {
-            if let action = triggeredEffect(by: card, player: player, state: state) {
-                triggered.append(action)
-            }
+    for middleware in middleWares {
+        if let output = middleware.handle(action: action, state: state) {
+            return Just(output).eraseToAnyPublisher()
         }
     }
 
-    // just eliminated player
-    if case let .eliminate(player) = action {
-        let playerObj = state.player(player)
-        let cards = triggerableCardsOfEliminatedPlayer(playerObj)
-        for card in cards {
-            if let action = triggeredEffect(by: card, player: player, state: state) {
-                triggered.append(action)
-            }
+    return Empty().eraseToAnyPublisher()
+}
+
+protocol GameActionMiddleware {
+    func handle(action: GameAction, state: GameState) -> GameAction?
+}
+
+struct NextActionMiddleware: GameActionMiddleware {
+    func handle(action: GameAction, state: GameState) -> GameAction? {
+        switch action {
+        case .setGameOver,
+                .chooseOne,
+                .activateCards:
+            nil
+
+        default:
+            state.queue.first
         }
     }
+}
 
-    // Ignore empty
-    guard triggered.isNotEmpty else {
+struct TriggeredEffectsMiddleware: GameActionMiddleware {
+    func handle(action: GameAction, state: GameState) -> GameAction? {
+        var triggered: [GameAction] = []
+
+        // active players
+        for player in state.playOrder {
+            let playerObj = state.player(player)
+            let cards = triggerableCardsOfActivePlayer(playerObj)
+            for card in cards {
+                if let action = triggeredEffect(by: card, player: player, state: state) {
+                    triggered.append(action)
+                }
+            }
+        }
+
+        // just eliminated player
+        if case let .eliminate(player) = action {
+            let playerObj = state.player(player)
+            let cards = triggerableCardsOfEliminatedPlayer(playerObj)
+            for card in cards {
+                if let action = triggeredEffect(by: card, player: player, state: state) {
+                    triggered.append(action)
+                }
+            }
+        }
+
+        // Ignore empty
+        guard triggered.isNotEmpty else {
+            return nil
+        }
+
+        return .group(triggered)
+    }
+
+    private func triggeredEffect(by card: String, player: String, state: GameState) -> GameAction? {
+        let cardName = card.extractName()
+        guard let cardObj = state.cardRef[cardName] else {
+            return nil
+        }
+
+        let playReqContext = PlayReqContext(actor: player)
+        for (playReq, effect) in cardObj.rules where  playReq.match(state: state, ctx: playReqContext) {
+            let linkedAction = linkedActionForTriggeredEffect(state: state)
+            let ctx = EffectContext(actor: player, card: card, linkedAction: linkedAction)
+            return GameAction.effect(effect, ctx: ctx)
+        }
+
         return nil
     }
 
-    return .group(triggered)
-}
+    private func triggerableCardsOfActivePlayer(_ playerObj: Player) -> [String] {
+        playerObj.inPlay.cards
+        + playerObj.abilities
+        + playerObj.hand.cards.filter { $0.starts(with: "missed") }
+    }
 
-private func triggeredEffect(by card: String, player: String, state: GameState) -> GameAction? {
-    let cardName = card.extractName()
-    guard let cardObj = state.cardRef[cardName] else {
+    private func triggerableCardsOfEliminatedPlayer(_ playerObj: Player) -> [String] {
+        playerObj.abilities
+    }
+
+    private func linkedActionForTriggeredEffect(state: GameState) -> GameAction? {
+        if let event = state.event,
+           case let .effect(cardEffect, _) = event,
+           case .shoot = cardEffect,
+           let nextAction = state.queue.first,
+           case .damage = nextAction {
+            return nextAction
+        }
+
         return nil
     }
+}
 
-    let playReqContext = PlayReqContext(actor: player)
-    for (playReq, effect) in cardObj.rules where  playReq.match(state: state, ctx: playReqContext) {
-        let linkedAction = linkedActionForTriggeredEffect(state: state)
-        let ctx = EffectContext(actor: player, card: card, linkedAction: linkedAction)
-        return GameAction.effect(effect, ctx: ctx)
+struct ActivateCardsMiddleware: GameActionMiddleware {
+    func handle(action: GameAction, state: GameState) -> GameAction? {
+        guard state.queue.isEmpty,
+              state.isOver == nil,
+              state.chooseOne == nil,
+              state.active == nil,
+              let player = state.turn else {
+            return nil
+        }
+
+        var activeCards: [String] = []
+        let playerObj = state.player(player)
+        for card in (playerObj.hand.cards + playerObj.abilities)
+        where GameAction.validatePlay(card: card, player: player, state: state) {
+            activeCards.append(card)
+        }
+
+        // Ignore empty
+        guard activeCards.isNotEmpty else {
+            return nil
+        }
+
+        return .activateCards(player: player, cards: activeCards)
+    }
+}
+
+struct GameOverMiddleware: GameActionMiddleware {
+    func handle(action: GameAction, state: GameState) -> GameAction? {
+        guard case .eliminate = action,
+              let winner = evaluateWinner(state: state)else {
+            return nil
+        }
+
+        return .setGameOver(winner: winner)
     }
 
-    return nil
-}
+    private func evaluateWinner(state: GameState) -> String? {
+        if state.playOrder.count == 1 {
+            return state.playOrder[0]
+        }
 
-private func triggerableCardsOfActivePlayer(_ playerObj: Player) -> [String] {
-    playerObj.inPlay.cards
-    + playerObj.abilities
-    + playerObj.hand.cards.filter { $0.starts(with: "missed") }
-}
-
-private func triggerableCardsOfEliminatedPlayer(_ playerObj: Player) -> [String] {
-    playerObj.abilities
-}
-
-private func linkedActionForTriggeredEffect(state: GameState) -> GameAction? {
-    if let event = state.event,
-       case let .effect(cardEffect, _) = event,
-       case .shoot = cardEffect,
-       let nextAction = state.queue.first,
-       case .damage = nextAction {
-        return nextAction
-    }
-
-    return nil
-}
-
-// MARK: - Active cards
-
-private func evaluateActiveCards(state: GameState) -> GameAction? {
-    guard state.queue.isEmpty,
-          state.isOver == nil,
-          state.chooseOne == nil,
-          state.active == nil,
-          let player = state.turn else {
         return nil
     }
-
-    var activeCards: [String] = []
-    let playerObj = state.player(player)
-    for card in (playerObj.hand.cards + playerObj.abilities)
-    where GameAction.validatePlay(card: card, player: player, state: state) {
-        activeCards.append(card)
-    }
-
-    // Ignore empty
-    guard activeCards.isNotEmpty else {
-        return nil
-    }
-
-    return GameAction.activateCards(player: player, cards: activeCards)
-}
-
-// MARK: - Game over
-
-private func evaluateGameOver(action: GameAction, state: GameState) -> GameAction? {
-    guard case .eliminate = action,
-          let winner = evaluateWinner(state: state)else {
-        return nil
-    }
-
-    return .setGameOver(winner: winner)
-}
-
-private func evaluateWinner(state: GameState) -> String? {
-    if state.playOrder.count == 1 {
-        return state.playOrder[0]
-    }
-
-    return nil
 }
