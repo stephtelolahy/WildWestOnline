70e07d38aec2a85400c0db2fcc4b1e02cbc289a8
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-11-04 23:00:21 +0100

    Test encoding

---
+++ GameKit/Tests/InventoryTests/Figures/ElGringoSpec.swift
@@ -0,0 +1,36 @@
+//
+//  ElGringoSpec.swift
+//
+//
+//  Created by Hugues Telolahy on 04/11/2023.
+//
+import Quick
+import Nimble
+import Game
+
+final class ElGringoSpec: QuickSpec {
+    override func spec() {
+        describe("ElGringo") {
+            context("being damaged") {
+                context("offender has several hand cards") {
+                    it("should steal one card") {
+                        
+                    }
+                }
+                
+                context("offender has one hand card") {
+                    it("should steal that card") {
+                        
+                    }
+                }
+                
+                context("offender has no cards") {
+                    it("should do nothing") {
+                        
+                    }
+                }
+                
+            }
+        }
+    }
+}
