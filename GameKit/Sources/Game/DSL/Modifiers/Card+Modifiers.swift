ff2ed4d045c8056a27ec7b5d1d47bd8010931c4a
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-11-05 17:55:20 +0100

    Card unique constructor

--- GameKit/Sources/Game/DSL/Modifiers/Card+Modifiers.swift
+++ GameKit/Sources/Game/DSL/Modifiers/Card+Modifiers.swift
@@ -12,21 +12,11 @@ public extension Card {
     init(
         _ name: String,
         attributes: [AttributeKey: Int] = [:],
+        prototype: Card? = nil,
         @CardRuleBuilder content: () -> [CardRule] = { [] }
     ) {
         self.name = name
         self.attributes = attributes
-        self.rules = content()
-    }
-
-    init(
-        _ name: String,
-        attributes: [AttributeKey: Int] = [:],
-        prototype: Card,
-        @CardRuleBuilder content: () -> [CardRule] = { [] }
-    ) {
-        self.name = name
-        self.attributes = attributes
-        self.rules = prototype.rules + content()
+        self.rules = (prototype?.rules ?? []) + content()
     }
 }
