9a8d555d9cbc4eed0f4fa941a4bb537ab2bce4a9
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-11-12 01:07:56 +0100

    Fix onHandEmpty

--- GameKit/Sources/Game/Reducer/PlayReq+Matcher/OnHandEmpty.swift
+++ GameKit/Sources/Game/Reducer/PlayReq+Matcher/OnHandEmpty.swift
@@ -8,22 +8,26 @@
 struct OnHandEmpty: PlayReqMatcher {
     func match(state: GameState, ctx: PlayReqContext) -> Bool {
         if case let .playImmediate(_, _, player) = state.event,
-           player == ctx.actor {
+           player == ctx.actor,
+           state.player(ctx.actor).hand.cards.isEmpty {
             return true
         }
 
         if case let .playHandicap(_, _, player) = state.event,
-           player == ctx.actor {
+           player == ctx.actor,
+           state.player(ctx.actor).hand.cards.isEmpty {
             return true
         }
 
         if case let .discardHand(_, player) = state.event,
-           player == ctx.actor {
+           player == ctx.actor,
+           state.player(ctx.actor).hand.cards.isEmpty {
             return true
         }
 
         if case let .stealHand(_, target, _) = state.event,
-           target == ctx.actor {
+           target == ctx.actor,
+           state.player(ctx.actor).hand.cards.isEmpty {
             return true
         }
 
