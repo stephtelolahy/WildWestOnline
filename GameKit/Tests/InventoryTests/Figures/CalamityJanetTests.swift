25b0711ae55a2e9fa352fba71699381ca38783ee
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-11-20 23:52:41 +0100

    wip

--- GameKit/Tests/InventoryTests/Figures/CalamityJanetTests.swift
+++ GameKit/Tests/InventoryTests/Figures/CalamityJanetTests.swift
@@ -34,5 +34,33 @@ final class CalamityJanetTests: XCTestCase {
         ])
     }
 
-    func test_calamityJanetPlayingBang_shouldPlayAsMissed() throws {}
+    func test_calamityJanetBeingShot_holdingBang_shouldAskToPlayAsMissed() throws {
+        // Given
+        let state = GameState.makeBuilderWithCardRef()
+            .withPlayer("p1") {
+                $0.withHand([.bang])
+                    .withAttributes([.calamityJanet: 0, .bangsPerTurn: 1, .weapon: 1])
+            }
+            .withPlayer("p2") {
+                $0.withHand(["bang-1", "bang-2"])
+                    .withAttributes([.activateCounterCardsOnShot: 0])
+            }
+            .build()
+
+        // When
+        let action = GameAction.playImmediate(.bang, target: "p2", player: "p1")
+        let (result, _) = awaitAction(action, state: state, choose: ["bang-1"])
+
+        // Then
+        XCTAssertEqual(result, [
+            .playImmediate(.bang, target: "p2", player: "p1"),
+            .chooseOne(player: "p2", options: [
+                "bang-1": .play("bang-1", player: "p2"),
+                "bang-2": .play("bang-2", player: "p2"),
+                .pass: .group([])
+            ]),
+            .playImmediate("bang-1", player: "p2"),
+            .cancel(.damage(1, player: "p2"))
+        ])
+    }
 }
