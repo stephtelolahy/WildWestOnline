33431eecc805081aca2a3df14759b71b38235421
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-11-20 23:30:47 +0100

    wip

---
+++ GameKit/Tests/InventoryTests/Figures/CalamityJanetTests.swift
@@ -0,0 +1,15 @@
+//
+//  CalamityJanetTests.swift
+//  
+//
+//  Created by Hugues Telolahy on 20/11/2023.
+//
+
+import XCTest
+import Game
+import Inventory
+
+final class CalamityJanetTests: XCTestCase {
+
+    func test_calamityJanet_shouldShoot() throws {}
+}
