eb228db28eed504a6c0a3335886cadbf4cc92ae0
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-11-26 02:29:24 +0100

    wip

--- GameKit/Sources/Screen/GamePlay/GamePlayView.swift
+++ GameKit/Sources/Screen/GamePlay/GamePlayView.swift
@@ -17,32 +17,51 @@ struct GamePlayView: View {
     }
 
     var body: some View {
-        VStack(alignment: .leading) {
-            Button {
-                withAnimation {
-                    store.dispatch(AppAction.dismissScreen(.game))
-                }
-            } label: {
-                HStack {
-                    Image(systemName: "hand.point.left.fill")
-                    Text("game.quit.button", bundle: .module)
-                }
-                .foregroundColor(.accentColor)
-            }
-            .padding()
-            List {
-                Section {
-                    ForEach(state.players) {
-                        PlayerView(player: $0)
+        ZStack {
+            VStack(alignment: .leading) {
+                Button {
+                    withAnimation {
+                        store.dispatch(AppAction.dismissScreen(.game))
+                    }
+                } label: {
+                    HStack {
+                        Image(systemName: "hand.point.left.fill")
+                        Text("game.quit.button", bundle: .module)
                     }
                 }
-            }
-            Text(String(format: String(localized: "game.message", bundle: .module), state.message ?? ""))
-                .font(.subheadline)
-                .foregroundColor(.accentColor)
                 .padding()
+                List {
+                    Section {
+                        ForEach(state.players) {
+                            PlayerView(player: $0)
+                        }
+                    }
+                }
+                Text(String(format: String(localized: "game.message", bundle: .module), state.message ?? ""))
+                    .font(.subheadline)
+                    .foregroundColor(.accentColor)
+                    .padding()
+            }
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .overlay(alignment: .bottomTrailing) {
+            floatingButton
+        }
+    }
+
+    private var floatingButton: some View {
+        Button {
+            // Action
+        } label: {
+            // 1
+            Image(systemName: "plus")
+                .font(.title.weight(.semibold))
+                .padding()
+                .background(Color.accentColor)
+                .foregroundStyle(.white)
+                .clipShape(Circle())
+        }
+        .padding()
     }
 }
 
