21ce7639227c4ff880694c51be2ecf82c06d263e
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-12-11 08:35:23 +0100

    wip

--- GameKit/Sources/App/AppState.swift
+++ GameKit/Sources/App/AppState.swift
@@ -36,35 +36,11 @@ public extension AppState {
     static let reducer: Reducer<Self> = { state, action in
         var state = state
 
+        // Update active game
+        state = gameReducer(state, action)
+
         // Update visible screens
-        switch action {
-        case let NavAction.showScreen(screen, transition):
-            switch transition {
-            case .push:
-                state.screens.append(screen)
-
-            case .replace:
-                state.screens = [screen]
-            }
-
-            // <create game>
-            if case .game = screen {
-                state.game = state.createGame()
-            }
-            // </create game>
-
-        case NavAction.dismiss:
-            // <delete game>
-            if case .game = state.screens.last {
-                state.game = nil
-            }
-            // </delete game>
-
-            state.screens.removeLast()
-
-        default:
-            break
-        }
+        state.screens = screenReducer(state.screens, action)
 
         // Reduce game
         if let gameState = state.game {
@@ -80,14 +56,51 @@ public extension AppState {
 }
 
 private extension AppState {
+    static let screenReducer: Reducer<[Screen]> = { state, action in
+        guard let action = action as? NavAction else {
+            return state
+        }
+
+        var state = state
+        switch action {
+        case let .showScreen(screen, transition):
+            switch transition {
+            case .push:
+                state.append(screen)
+
+            case .replace:
+                state = [screen]
+            }
+
+        case .dismiss:
+            state.removeLast()
+        }
+
+        return state
+    }
+
+    static let gameReducer: Reducer<AppState> = { state, action in
+        var state = state
+
+        if case let NavAction.showScreen(screen, _) = action,
+           screen == .game {
+            state.game = state.createGame()
+        }
+
+        if case NavAction.dismiss = action,
+           state.screens.last == .game {
+            state.game = nil
+        }
+
+        return state
+    }
+
     func createGame() -> GameState {
         var game = Inventory.createGame(playersCount: settings.playersCount)
-
         var manualPlayer: String?
         if !settings.simulation {
             manualPlayer = game.playOrder[0]
         }
-
         game.playMode = game.startOrder.reduce(into: [:]) {
             $0[$1] = $1 == manualPlayer ? .manual : .auto
         }
