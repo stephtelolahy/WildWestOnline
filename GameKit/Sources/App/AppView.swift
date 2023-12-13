5abb6496bbad3d63d0189405527e9e52876ea09b
Stephano Telolahy <stephano.telolahy@gmail.com>
2023-12-13 01:28:48 +0100

    wip

--- GameKit/Sources/App/AppView.swift
+++ GameKit/Sources/App/AppView.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Hugues Telolahy on 02/04/2023.
 //
-// swiftlint:disable type_contents_order
+// swiftlint:disable type_contents_order no_magic_numbers
 
 import Game
 import GameUI
