77cb1c29f22a7f87cafa2daccc9dc0ff6e6157bf
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-01-06 00:49:22 +0100

    wip

---
+++ GameKit/Tests/GameTests/Mechanics/PassInPlayTests.swift
@@ -0,0 +1,30 @@
+//
+//  PassInPlayTests.swift
+//  
+//
+//  Created by Hugues Telolahy on 06/01/2024.
+//
+
+import Game
+import XCTest
+
+final class PassInPlayTests: XCTestCase {
+    func test_passInPlay_shouldRemoveCardFromInPlay() {
+        // Given
+        let state = GameState.makeBuilder()
+            .withPlayer("p1") {
+                $0.withInPlay(["c1", "c2"])
+            }
+            .withPlayer("p2")
+            .build()
+
+        // When
+        let action = GameAction.passInPlay("c1", target: "p2", player: "p1")
+        let result = GameState.reducer(state, action)
+
+        // Then
+        XCTAssertEqual(result.player("p1").inPlay, ["c2"])
+        XCTAssertEqual(result.player("p2").inPlay, ["c1"])
+    }
+
+}
