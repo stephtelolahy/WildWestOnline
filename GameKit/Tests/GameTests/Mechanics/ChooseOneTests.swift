47adacea839ddb9efcc1557083f1e551d97af3da
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-01-06 00:21:19 +0100

    wip

---
+++ GameKit/Tests/GameTests/Mechanics/ChooseOneTests.swift
@@ -0,0 +1,52 @@
+//
+//  ChooseOneTests.swift
+//  
+//
+//  Created by Hugues Telolahy on 06/01/2024.
+//
+
+import Game
+import XCTest
+
+final class ChooseOneTests: XCTestCase {
+    func test_dispatch_withWaitedAction_shouldRemoveWaitingState() {
+        // Given
+        let state = GameState.makeBuilder()
+            .withPlayer("p1") {
+                $0.withHand(["c1", "c2", "c3"])
+            }
+            .withChooseOne("p1", options: [
+                "c1": .discardHand("c1", player: "p1"),
+                "c2": .discardHand("c2", player: "p1")
+            ])
+            .build()
+
+        // When
+        let action = GameAction.discardHand("c1", player: "p1")
+        let result = GameState.reducer(state, action)
+
+        // Then
+        XCTAssertEqual(result.chooseOne, [:])
+    }
+
+    func test_dispatch_withNonWaitedAction_shouldThrowError() {
+        // Given
+        let state = GameState.makeBuilder()
+            .withPlayer("p1") {
+                $0.withHand(["c1", "c2", "c3"])
+            }
+            .withChooseOne("p1", options: [
+                "c1": .discardHand("c1", player: "p1"),
+                "c2": .discardHand("c2", player: "p1")
+            ])
+            .build()
+
+        // When
+        let action = GameAction.discardHand("c3", player: "p1")
+        let result = GameState.reducer(state, action)
+
+        // Then
+        XCTAssertFalse(result.chooseOne.isEmpty)
+        XCTAssertEqual(result.error, .unwaitedAction)
+    }
+}
