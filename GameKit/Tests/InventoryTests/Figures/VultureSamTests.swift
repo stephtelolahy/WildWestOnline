fce1fe970e24df91cee17291f1bf215b7fd9b1b5
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-01-06 14:41:56 +0100

    wip

---
+++ GameKit/Tests/InventoryTests/Figures/VultureSamTests.swift
@@ -0,0 +1,38 @@
+//
+//  VultureSamTests.swift
+//  
+//
+//  Created by Hugues Stephano TELOLAHY on 06/01/2024.
+//
+
+import Game
+import Inventory
+import XCTest
+
+final class VultureSamTests: XCTestCase {
+    func test_VultureSam_anotherPlayerEliminated_shouldDrawItsCard() {
+        // Given
+        let state = GameState.makeBuilderWithCardRef()
+            .withPlayer("p1") {
+                $0.withAbilities([.vultureSam])
+            }
+            .withPlayer("p2") {
+                $0.withAbilities([.discardCardsOnEliminated])
+                    .withHand(["c1"])
+                    .withInPlay(["c2"])
+            }
+            .withPlayer("p3")
+            .build()
+
+        // When
+        let action = GameAction.eliminate(player: "p2")
+        let (result, _) = self.awaitAction(action, state: state)
+
+        // Then
+        XCTAssertEqual(result, [
+            .eliminate(player: "p2"),
+            .drawInPlay("c2", target: "p2", player: "p1"),
+            .drawHand("c1", target: "p2", player: "p1")
+        ])
+    }
+}
