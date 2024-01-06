6e8c85aba06f2021348cdddf99eea6c7bbebb9f9
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-01-06 11:34:29 +0100

    wip

---
+++ GameKit/Tests/InventoryTests/Collectible/MustangTests.swift
@@ -0,0 +1,33 @@
+//
+//  MustangTests.swift
+//  
+//
+//  Created by Hugues Stephano TELOLAHY on 06/01/2024.
+//
+
+import Game
+import XCTest
+
+final class MustangTests: XCTestCase {
+    func test_playMustang_shouldEquipAndSetAttribute() {
+        // Given
+        let state = GameState.makeBuilderWithCardRef()
+            .withPlayer("p1") {
+                $0.withHand([.mustang])
+                    .withAbilities([.updateAttributesOnChangeInPlay])
+            }
+            .build()
+
+        // When
+        let action = GameAction.play(.mustang, player: "p1")
+        let (result, _) = self.awaitAction(action, state: state)
+
+        // Then
+        XCTAssertEqual(result, [
+            .play(.mustang, player: "p1"),
+            .equip(.mustang, player: "p1"),
+            .setAttribute(.mustang, value: 1, player: "p1")
+        ])
+    }
+
+}
