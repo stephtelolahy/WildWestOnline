35c45c9044891dd3454636cd40591cf248e2b292
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-01-06 11:17:16 +0100

    wip

---
+++ GameKit/Tests/InventoryTests/Collectible/SaloonTests.swift
@@ -0,0 +1,65 @@
+//
+//  SaloonTests.swift
+//
+//
+//  Created by Hugues Stephano TELOLAHY on 06/01/2024.
+//
+// swiftlint:disable no_magic_numbers
+
+import Game
+import XCTest
+
+final class SaloonTests: XCTestCase {
+    func test_playSaloon_withSomePlayersDamaged_shouldHealOneLifePoint() {
+        // Given
+        let state = GameState.makeBuilderWithCardRef()
+            .withPlayer("p1") {
+                $0.withHand([.saloon])
+                    .withHealth(4)
+                    .withAttributes([.maxHealth: 4])
+            }
+            .withPlayer("p2") {
+                $0.withHealth(2)
+                    .withAttributes([.maxHealth: 4])
+            }
+            .withPlayer("p3") {
+                $0.withHealth(3)
+                    .withAttributes([.maxHealth: 4])
+            }
+            .build()
+
+        // When
+        let action = GameAction.play(.saloon, player: "p1")
+        let (result, _) = self.awaitAction(action, state: state)
+
+        // Then
+        XCTAssertEqual(result, [
+            .play(.saloon, player: "p1"),
+            .discardPlayed(.saloon, player: "p1"),
+            .heal(1, player: "p2"),
+            .heal(1, player: "p3")
+        ])
+    }
+
+    func test_playSaloon_withoutPlayerDamaged_shouldThrowError() {
+        // Given
+        let state = GameState.makeBuilderWithCardRef()
+            .withPlayer("p1") {
+                $0.withHand([.saloon])
+                    .withHealth(4)
+                    .withAttributes([.maxHealth: 4])
+            }
+            .withPlayer("p2") {
+                $0.withHealth(3)
+                    .withAttributes([.maxHealth: 3])
+            }
+            .build()
+
+        // When
+        let action = GameAction.play(.saloon, player: "p1")
+        let (_, error) = self.awaitAction(action, state: state)
+
+        // Then
+        XCTAssertEqual(error, .noPlayer(.damaged))
+    }
+}
