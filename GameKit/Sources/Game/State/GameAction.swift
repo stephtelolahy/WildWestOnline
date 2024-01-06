7c876340209cf8adfb6a52a3f1eb0ed5a97464b2
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-01-06 14:47:04 +0100

    wip

--- GameKit/Sources/Game/State/GameAction.swift
+++ GameKit/Sources/Game/State/GameAction.swift
@@ -83,6 +83,7 @@ public indirect enum GameAction: Action, Codable, Equatable, DocumentConvertible
     case cancel(Self)
 
     /// Expose a choice
+    @available(*, deprecated, message: "Hide option details")
     case chooseOne([String: Self], player: String)
 
     /// Expose active cards
