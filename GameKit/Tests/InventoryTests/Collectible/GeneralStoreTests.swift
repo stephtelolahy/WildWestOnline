a2402d69f1818d81b6182cb2603413f4235d3a1e
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-01-06 16:39:21 +0100

    wip

---
+++ GameKit/Tests/InventoryTests/Collectible/GeneralStoreTests.swift
@@ -0,0 +1,49 @@
+//
+//  GeneralStoreTests.swift
+//
+//
+//  Created by Hugues Stephano TELOLAHY on 06/01/2024.
+//
+
+import Game
+import XCTest
+
+final class GeneralStoreTests: XCTestCase {
+
+    func test_playingGeneralStore_threePlayers_shouldAllowEachPlayerToChooseACard() {
+        // Given
+        let state = GameState.makeBuilderWithCardRef()
+            .withPlayer("p1") {
+                $0.withHand([.generalStore])
+            }
+            .withPlayer("p2")
+            .withPlayer("p3")
+            .withDeck(["c1", "c2", "c3"])
+            .build()
+
+        // When
+        let action = GameAction.play(.generalStore, player: "p1")
+        let (result, _) = self.awaitAction(action, state: state, choose: ["c1", "c2"])
+
+        // Then
+        XCTAssertEqual(result, [
+            .play(.generalStore, player: "p1"),
+            .discardPlayed(.generalStore, player: "p1"),
+            .discover,
+            .discover,
+            .discover,
+            .chooseOne([
+                "c1": .drawArena("c1", player: "p1"),
+                "c2": .drawArena("c2", player: "p1"),
+                "c3": .drawArena("c3", player: "p1")
+            ], player: "p1"),
+            .drawArena("c1", player: "p1"),
+            .chooseOne([
+                "c2": .drawArena("c2", player: "p2"),
+                "c3": .drawArena("c3", player: "p2")
+            ], player: "p2"),
+            .drawArena("c2", player: "p2"),
+            .drawArena("c3", player: "p3")
+        ])
+    }
+}
