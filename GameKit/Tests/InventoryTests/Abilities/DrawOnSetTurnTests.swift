c4399c8a5b283a8bad2baa9f1a1b87bfa7f77fda
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-01-06 07:37:40 +0100

    wip

---
+++ GameKit/Tests/InventoryTests/Abilities/DrawOnSetTurnTests.swift
@@ -0,0 +1,57 @@
+//
+//  DrawOnSetTurnTests.swift
+//  
+//
+//  Created by Hugues Stephano TELOLAHY on 06/01/2024.
+//
+// swiftlint:disable no_magic_numbers
+
+import Game
+import XCTest
+
+final class DrawOnSetTurnTests: XCTestCase {
+    func test_setTurn_with2StartTurnCards_shouldDraw2Cards() {
+        // Given
+        let state = GameState.makeBuilderWithCardRef()
+            .withPlayer("p1") {
+                $0.withAbilities([.drawOnSetTurn])
+                    .withAttributes([.startTurnCards: 2])
+            }
+            .withDeck(["c1", "c2"])
+            .build()
+
+        // When
+        let action = GameAction.setTurn(player: "p1")
+        let (result, _) = self.awaitAction(action, state: state)
+
+        // Then
+        XCTAssertEqual(result, [
+            .setTurn(player: "p1"),
+            .drawDeck(player: "p1"),
+            .drawDeck(player: "p1")
+        ])
+    }
+
+    func test_setTurn_with3StartTurnCards_shouldDraw3Cards() {
+        // Given
+        let state = GameState.makeBuilderWithCardRef()
+            .withPlayer("p1") {
+                $0.withAbilities([.drawOnSetTurn])
+                    .withAttributes([.startTurnCards: 3])
+            }
+            .withDeck(["c1", "c2"])
+            .build()
+
+        // When
+        let action = GameAction.setTurn(player: "p1")
+        let (result, _) = self.awaitAction(action, state: state)
+
+        // Then
+        XCTAssertEqual(result, [
+            .setTurn(player: "p1"),
+            .drawDeck(player: "p1"),
+            .drawDeck(player: "p1"),
+            .drawDeck(player: "p1")
+        ])
+    }
+}
