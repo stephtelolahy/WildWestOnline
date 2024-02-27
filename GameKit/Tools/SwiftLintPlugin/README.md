d03d9b645fe862ae345c6f109cd9866150e02d60
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-02-27 06:39:43 +0100

    Use local SwiftLint

---
+++ GameKit/Tools/SwiftLintPlugin/README.md
@@ -0,0 +1,6 @@
+#  SwiftLint
+
+## Setup artifact bundle
+- Download from https://github.com/realm/SwiftLint/releases/download/0.54.0/SwiftLintBinary-macos.artifactbundle.zip
+- Unzip 
+- Move SwiftLintBinary.artifactbundle folder in Tools/
---
+++ GameKit/Tools/SwiftLintPlugin/Sources/main.swift
@@ -0,0 +1,57 @@
+//
+//  main.swift
+//  
+//
+//  Created by Stephano Hugues TELOLAHY on 27/02/2024.
+//
+
+import Foundation
+import PackagePlugin
+
+@main
+struct SwiftLintPlugin: BuildToolPlugin {
+    func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
+        if ProcessInfo.processInfo.environment["DISABLE_SWIFTLINT"] != nil {
+            return []
+        }
+        return [
+            .buildCommand(
+                displayName: "Running SwiftLint for \(target.name)",
+                executable: try context.tool(named: "swiftlint").path,
+                arguments: [
+                    "lint",
+                    "--config",
+                    "\(context.package.directory.string)/.swiftlint.yml",
+                    "--cache-path",
+                    "\(context.pluginWorkDirectory.string)/cache",
+                    target.directory.string
+                ],
+                environment: [:]
+            )
+        ]
+    }
+}
+
+#if canImport(XcodeProjectPlugin)
+import XcodeProjectPlugin
+
+extension SwiftLintPlugin: XcodeBuildToolPlugin {
+    func createBuildCommands(context: XcodePluginContext, target: XcodeTarget) throws -> [Command] {
+        [
+            .buildCommand(
+                displayName: "Running SwiftLint for \(target.displayName)",
+                executable: try context.tool(named: "swiftlint").path,
+                arguments: [
+                    "lint",
+                    "--config",
+                    "\(context.xcodeProject.directory.string)/.swiftlint.yml",
+                    "--cache-path",
+                    "\(context.pluginWorkDirectory.string)/cache",
+                    context.xcodeProject.directory.string
+                ],
+                environment: [:]
+            )
+        ]
+    }
+}
+#endif
