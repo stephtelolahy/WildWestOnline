db6f8514efe9a0b8b520f8c4885a4386ba252d31
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-06-23 02:08:46 +0200

    refactor: chain middleware

--- WildWestOnline/Core/Game/Sources/Middleware/UpdateGameMiddleware.swift
+++ WildWestOnline/Core/Game/Sources/Middleware/UpdateGameMiddleware.swift
@@ -10,12 +10,12 @@ import Redux
 /// Game loop features
 public extension Middlewares {
     static func updateGame() -> Middleware<GameState> {
-        Middlewares.compose([
-            Middlewares.checkGameOver(),
-            Middlewares.triggerCardEffects(),
-            Middlewares.processSequence(),
-            Middlewares.activatePlayableCards(),
-            Middlewares.playAIMoves(strategy: RandomAIStrategy())
+        chain([
+            checkGameOver(),
+            triggerCardEffects(),
+            processSequence(),
+            activatePlayableCards(),
+            playAIMoves(strategy: RandomAIStrategy())
         ])
     }
 }
