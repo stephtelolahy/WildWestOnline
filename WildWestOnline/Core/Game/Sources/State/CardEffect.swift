942142b033c4c2aaf8fbd344c91ca993e2bad034
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-07-13 18:45:36 +0200

    Update image

--- WildWestOnline/Core/Game/Sources/State/CardEffect.swift
+++ WildWestOnline/Core/Game/Sources/State/CardEffect.swift
@@ -113,3 +113,46 @@ public indirect enum CardEffect: Codable, Equatable {
     /// Runs the action matching `EffectContext.chosenOption`
     case matchAction([String: GameAction])
 }
+
+public extension CardEffect {
+    static let nothing: Self = .group([])
+
+    static func group(@CardEffectsBuilder content: () -> [Self]) -> Self {
+        .group(content())
+    }
+
+    func `repeat`(_ times: ArgNum) -> Self {
+        .repeat(times, effect: self)
+    }
+
+    func `repeat`(_ times: Int) -> Self {
+        .repeat(.exact(times), effect: self)
+    }
+
+    func target(_ target: ArgPlayer) -> Self {
+        .target(target, effect: self)
+    }
+
+    func force(otherwise: Self) -> Self {
+        .force(self, otherwise: otherwise)
+    }
+
+    func challenge(_ challenger: ArgPlayer, otherwise: Self) -> Self {
+        .challenge(challenger, effect: self, otherwise: otherwise)
+    }
+
+    func ignoreError() -> Self {
+        .ignoreError(self)
+    }
+
+    func on(_ playReqs: [PlayReq]) -> CardRule {
+        .init(effect: self, playReqs: playReqs)
+    }
+}
+
+@resultBuilder
+public enum CardEffectsBuilder {
+    public static func buildBlock(_ components: CardEffect...) -> [CardEffect] {
+        components
+    }
+}
