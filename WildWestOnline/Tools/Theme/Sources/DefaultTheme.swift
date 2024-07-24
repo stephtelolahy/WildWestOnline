86b5b4f3d29480379f4a4e9d3a71517c940ddd8e
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-07-24 07:53:33 +0200

    Completed

---
+++ WildWestOnline/Tools/Theme/Sources/DefaultTheme.swift
@@ -0,0 +1,22 @@
+//
+//  DefaultTheme.swift
+//  
+//
+//  Created by Stephano Hugues TELOLAHY on 24/07/2024.
+//
+// swiftlint:disable no_magic_numbers
+import SwiftUI
+
+struct DefaultTheme: AppTheme {
+    var backgroundView = Color(
+        red: 249.0 / 255.0,
+        green: 215.0 / 255.0,
+        blue: 160.0 / 255.0
+    )
+
+    var accentColor = Color(
+        red: 148.0 / 255.0,
+        green: 82.0 / 255.0,
+        blue: 0.0 / 255.0
+    )
+}
