f17ca0c80d6eb2417adcaccd6991b3ea9f044588
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-08-17 00:41:24 +0200

    wip

--- WildWestOnline/Utilities/Redux/Sources/Store.swift
+++ WildWestOnline/Utilities/Redux/Sources/Store.swift
@@ -3,6 +3,18 @@ import Foundation
 import SwiftUI
 // swiftlint:disable private_subject
 
+/// ``Reducer`` is a pure function that takes an action and the current state to calculate the new state.
+public typealias Reducer<State, Action> = (State, Action) throws -> State
+public typealias SelectorReducer<InputState, OutputState, Action> = (InputState, Action) throws -> OutputState
+
+/// ``Middleware`` is a plugin, or a composition of several plugins,
+/// that are assigned to the app global  state pipeline in order to
+/// handle each action received action, to execute side-effects in response, and eventually dispatch more actions
+public typealias Middleware<State, Action> = (State, Action) async -> Action?
+
+/// Namespace for Middlewares
+public enum Middlewares {}
+
 /// ``Store`` is a base class that can be used to create the main store of an app, using the redux pattern.
 /// It defines two roles of a "Store":
 /// - receive/distribute `Action`;
@@ -36,52 +48,20 @@ public class Store<State, Action>: ObservableObject {
             event.send(action)
 
             for middleware in middlewares {
-                Future<Action, Never>.create(from: middleware, state: newState, action: action)
-                    .compactMap { $0 }
-                    .subscribe(on: middlewareSerialQueue)
-                    .receive(on: RunLoop.main)
-                    .sink(receiveValue: dispatch)
-                    .store(in: &subscriptions)
+                Future<Action?, Never> { promise in
+                    Task {
+                        let output = await middleware(newState, action)
+                        promise(.success(output))
+                    }
+                }
+                .compactMap { $0 }
+                .subscribe(on: middlewareSerialQueue)
+                .receive(on: RunLoop.main)
+                .sink(receiveValue: dispatch)
+                .store(in: &subscriptions)
             }
         } catch {
             self.error.send(error)
-        }
-    }
-}
-
-/// ``Reducer`` is a pure function that takes an action and the current state to calculate the new state.
-public typealias Reducer<State, Action> = (State, Action) throws -> State
-
-public typealias SelectorReducer<InputState, OutputState, Action> = (InputState, Action) throws -> OutputState
-
-/// ``Middleware`` is a plugin, or a composition of several plugins,
-/// that are assigned to the app global  state pipeline in order to
-/// handle each action received action, to execute side-effects in response, and eventually dispatch more actions
-public typealias Middleware<State, Action> = (State, Action) async -> Action?
-
-/// Namespace for Middlewares
-public enum Middlewares {}
-
-/// Class for holding value type
-public class ClassWrapper<T> {
-    public var value: T
-
-    public init(_ value: T) {
-        self.value = value
-    }
-}
-
-private extension Future {
-    static func create<State, Action>(
-        from middleware: @escaping Middleware<State, Action>,
-        state: State,
-        action: Action
-    ) -> Future<Action?, Never> {
-        Future<Action?, Never> { promise in
-            Task {
-                let output = await middleware(state, action)
-                promise(.success(output))
-            }
         }
     }
 }
