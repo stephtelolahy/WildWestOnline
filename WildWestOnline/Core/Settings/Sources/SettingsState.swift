3b839ea064339b2e5c8973ba0cac8c4bbefb28cb
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-09-15 01:25:25 +0200

    wip

--- WildWestOnline/Core/Settings/Sources/SettingsState.swift
+++ WildWestOnline/Core/Settings/Sources/SettingsState.swift
@@ -4,73 +4,12 @@
 //
 //  Created by Stephano Hugues TELOLAHY on 23/02/2024.
 //
-import Redux
 
 public struct SettingsState: Codable, Equatable {
     public var playersCount: Int
     public var waitDelayMilliseconds: Int
     public var simulation: Bool
     public var preferredFigure: String?
-}
-
-public extension SettingsState {
-    static let reducer: Reducer<Self, SettingsAction> = { state, action in
-        switch action {
-        case .updatePlayersCount:
-            try updatePlayersCountReducer(state, action)
-
-        case .updateWaitDelayMilliseconds:
-            try updateWaitDelayMillisecondsReducer(state, action)
-
-        case .toggleSimulation:
-            try toggleSimulationReducer(state, action)
-
-        case .updatePreferredFigure:
-            try updatePreferredFigureReducer(state, action)
-        }
-    }
-}
-
-private extension SettingsState {
-    static let updatePlayersCountReducer: Reducer<Self, SettingsAction> = { state, action in
-        guard case let .updatePlayersCount(value) = action else {
-            fatalError("unexpected")
-        }
-
-        var state = state
-        state.playersCount = value
-        return state
-    }
-
-    static let updateWaitDelayMillisecondsReducer: Reducer<Self, SettingsAction> = { state, action in
-        guard case let .updateWaitDelayMilliseconds(value) = action else {
-            fatalError("unexpected")
-        }
-
-        var state = state
-        state.waitDelayMilliseconds = value
-        return state
-    }
-
-    static let toggleSimulationReducer: Reducer<Self, SettingsAction> = { state, action in
-        guard case .toggleSimulation = action else {
-            fatalError("unexpected")
-        }
-
-        var state = state
-        state.simulation.toggle()
-        return state
-    }
-
-    static let updatePreferredFigureReducer: Reducer<Self, SettingsAction> = { state, action in
-        guard case let .updatePreferredFigure(value) = action else {
-            fatalError("unexpected")
-        }
-
-        var state = state
-        state.preferredFigure = value
-        return state
-    }
 }
 
 public extension SettingsState {
