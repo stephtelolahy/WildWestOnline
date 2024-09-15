5c846030c5c5c895d4733e85d3a697d59d0ffb8e
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-09-15 02:44:39 +0200

    wip

--- WildWestOnline/Core/App/Sources/AppState.swift
+++ WildWestOnline/Core/App/Sources/AppState.swift
@@ -4,10 +4,9 @@
 //
 //  Created by Stephano Hugues TELOLAHY on 23/02/2024.
 //
-import GameCore
 import NavigationCore
-import Redux
 import SettingsCore
+import GameCore
 
 /// Global app state
 /// Organize State Structure Based on Data Types, Not Components
@@ -28,79 +27,5 @@ public struct AppState: Codable, Equatable {
         self.settings = settings
         self.inventory = inventory
         self.game = game
-    }
-}
-
-public extension AppState {
-    static let reducer: Reducer<Self, Any> = { state, action in
-        var state = state
-        state.navigation = try NavigationState.reducer(state.navigation, action)
-
-        if let action = action as? SettingsAction {
-            state.settings = try SettingsState.reducer(state.settings, action)
-        }
-
-        if let action = action as? GameSetupAction {
-            state = try gameSetupReducer(state, action)
-        }
-
-        if let action = action as? GameAction {
-            state.game = try state.game.flatMap { try GameState.reducer($0, action) }
-        }
-
-        return state
-    }
-}
-
-private extension AppState {
-    static let gameSetupReducer: Reducer<Self, GameSetupAction> = { state, action in
-        switch action {
-        case .start:
-            try startReducer(state, action)
-
-        case .quit:
-            try quitReducer(state, action)
-        }
-    }
-
-    static let startReducer: Reducer<Self, GameSetupAction> = { state, action in
-        guard case .start = action else {
-            fatalError("unexpected")
-        }
-
-        var state = state
-        state.game = createGame(settings: state.settings, inventory: state.inventory)
-        // TODO: should emit navigation action through middleware
-        state.navigation.root.path.append(.game)
-        return state
-    }
-
-    static let quitReducer: Reducer<Self, GameSetupAction> = { state, action in
-        guard case .quit = action else {
-            fatalError("unexpected")
-        }
-
-        var state = state
-        // TODO: should emit navigation action through middleware
-        state.navigation.root.path.removeLast()
-        state.game = nil
-        return state
-    }
-
-    static func createGame(settings: SettingsState, inventory: Inventory) -> GameState {
-        var game = Setup.buildGame(
-            playersCount: settings.playersCount,
-            inventory: inventory,
-            preferredFigure: settings.preferredFigure
-        )
-
-        let manualPlayer: String? = settings.simulation ? nil : game.round.playOrder[0]
-        game.config.playMode = game.round.startOrder.reduce(into: [:]) {
-            $0[$1] = $1 == manualPlayer ? .manual : .auto
-        }
-
-        game.config.waitDelayMilliseconds = settings.waitDelayMilliseconds
-
-        return game
     }
 }
