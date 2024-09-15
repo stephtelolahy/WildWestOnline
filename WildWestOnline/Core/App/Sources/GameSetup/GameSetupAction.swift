163f2b69f93a856e2e34a372c32a9029d0c921f2
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-09-15 17:48:03 +0200

    add thunk

--- WildWestOnline/Core/App/Sources/GameSetup/GameSetupAction.swift
+++ WildWestOnline/Core/App/Sources/GameSetup/GameSetupAction.swift
@@ -4,9 +4,52 @@
 //
 //  Created by Stephano Hugues TELOLAHY on 23/06/2024.
 //
+// swiftlint:disable prefer_self_in_static_references
+
 import Redux
+import GameCore
+import SettingsCore
+import NavigationCore
 
 public enum GameSetupAction: Action {
-    case start
-    case quit
+    case setGame(GameState)
+    case unsetGame
+
+    public static let start: Thunk = { arg in
+        guard let state = arg.getState() as? AppState else {
+            fatalError("unexpected")
+        }
+
+        let newGame = AppState.createGame(
+            settings: state.settings,
+            inventory: state.inventory
+        )
+
+        arg.dispatch(GameSetupAction.setGame(newGame))
+        arg.dispatch(NavigationStackAction<RootDestination>.push(.game))
+    }
+
+    public static let quit: Thunk = { arg in
+        arg.dispatch(NavigationStackAction<RootDestination>.pop)
+        arg.dispatch(GameSetupAction.unsetGame)
+    }
+}
+
+private extension AppState {
+    static func createGame(settings: SettingsState, inventory: Inventory) -> GameState {
+        var game = Setup.buildGame(
+            playersCount: settings.playersCount,
+            inventory: inventory,
+            preferredFigure: settings.preferredFigure
+        )
+
+        let manualPlayer: String? = settings.simulation ? nil : game.round.playOrder[0]
+        game.playMode = game.round.startOrder.reduce(into: [:]) {
+            $0[$1] = $1 == manualPlayer ? .manual : .auto
+        }
+
+        game.waitDelayMilliseconds = settings.waitDelayMilliseconds
+
+        return game
+    }
 }
--- WildWestOnline/Core/App/Sources/GameSetup/GameSetupReducer.swift
+++ WildWestOnline/Core/App/Sources/GameSetup/GameSetupReducer.swift
@@ -11,58 +11,19 @@ import SettingsCore
 
 extension AppState {
     static let gameSetupReducer: Reducer<Self> = { state, action in
-        switch action {
-        case GameSetupAction.start:
-            try startReducer(state, action)
+        var state = state
 
-        case GameSetupAction.quit:
-            try quitReducer(state, action)
+        switch action {
+        case GameSetupAction.setGame(let game):
+            state.game = game
+
+        case GameSetupAction.unsetGame:
+            state.game = nil
 
         default:
-            state
-        }
-    }
-}
-
-private extension AppState {
-    static let startReducer: Reducer<Self> = { state, action in
-        guard case GameSetupAction.start = action else {
-            fatalError("unexpected")
+            break
         }
 
-        var state = state
-        state.game = createGame(settings: state.settings, inventory: state.inventory)
-        // TODO: should emit navigation action through middleware
-        state.navigation.root.path.append(.game)
         return state
-    }
-
-    static let quitReducer: Reducer<Self> = { state, action in
-        guard case GameSetupAction.quit = action else {
-            fatalError("unexpected")
-        }
-
-        var state = state
-        // TODO: should emit navigation action through middleware
-        state.navigation.root.path.removeLast()
-        state.game = nil
-        return state
-    }
-
-    static func createGame(settings: SettingsState, inventory: Inventory) -> GameState {
-        var game = Setup.buildGame(
-            playersCount: settings.playersCount,
-            inventory: inventory,
-            preferredFigure: settings.preferredFigure
-        )
-
-        let manualPlayer: String? = settings.simulation ? nil : game.round.playOrder[0]
-        game.playMode = game.round.startOrder.reduce(into: [:]) {
-            $0[$1] = $1 == manualPlayer ? .manual : .auto
-        }
-
-        game.waitDelayMilliseconds = settings.waitDelayMilliseconds
-
-        return game
     }
 }
