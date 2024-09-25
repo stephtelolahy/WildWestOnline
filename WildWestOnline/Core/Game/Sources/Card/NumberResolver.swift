ae658c8d420e19c3639c91d8c641511f8b64bbb6
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-09-25 09:42:35 +0200

    wip

--- WildWestOnline/Core/Game/Sources/Card/NumberResolver.swift
+++ WildWestOnline/Core/Game/Sources/Card/NumberResolver.swift
@@ -1,5 +1,5 @@
 //
-//  ArgNumberResolver.swift
+//  NumberResolver.swift
 //  WildWestOnline
 //
 //  Created by Hugues Stephano TELOLAHY on 25/09/2024.
@@ -7,6 +7,63 @@
 
 extension Effect.Selector.Number {
     func resolve(state: GameState, ctx: ResolvingEffect) throws -> Int {
-        2
+        try resolver.resolve(state: state, ctx: ctx)
+    }
+}
+
+private protocol NumberResolver {
+    func resolve(state: GameState, ctx: ResolvingEffect) throws -> Int
+}
+
+private extension Effect.Selector.Number {
+    var resolver: NumberResolver {
+        switch self {
+        case .activePlayers:
+            NumActivePlayers()
+        case .excessHand:
+            NumExcessHand()
+        case .wound:
+            NumWound()
+        case .damage:
+            NumDamage()
+        case .value(let number):
+            NumExact(number: number)
+        }
+    }
+
+    struct NumExact: NumberResolver {
+        let number: Int
+
+        func resolve(state: GameState, ctx: ResolvingEffect) throws -> Int {
+            number
+        }
+    }
+
+    struct NumActivePlayers: NumberResolver {
+        func resolve(state: GameState, ctx: ResolvingEffect) throws -> Int {
+            state.round.playOrder.count
+        }
+    }
+
+    struct NumExcessHand: NumberResolver {
+        func resolve(state: GameState, ctx: ResolvingEffect) throws -> Int {
+            let handCount = state.field.hand.get(ctx.actor).count
+            let handlLimit = state.player(ctx.actor).handLimitAtEndOfTurn
+            return max(handCount - handlLimit, 0)
+        }
+    }
+
+    struct NumWound: ArgNumResolver {
+        func resolve(state: GameState, ctx: EffectContext) throws -> Int {
+            let maxHealth = state.player(ctx.actor).attributes[.maxHealth]
+            let health = state.player(ctx.actor).health
+            return maxHealth - health
+        }
+    }
+}
+
+private extension Player {
+    var handLimitAtEndOfTurn: Int {
+        attributes[.handLimit] ?? health
     }
 }
