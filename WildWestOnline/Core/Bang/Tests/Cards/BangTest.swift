2470c4ce3ad2701bf9e5de437d8c30a18a6ffbf1
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-11-10 00:19:44 +0100

    wip

--- WildWestOnline/Core/Bang/Tests/Cards/BangTest.swift
+++ WildWestOnline/Core/Bang/Tests/Cards/BangTest.swift
@@ -53,48 +53,52 @@ struct BangTest {
             try await dispatchUntilCompleted(action, state: state)
         }
     }
+
     /*
-    func play_noLimitPerTurn_shouldAllowMultipleBang() async throws {
+    @Test func play_noLimitPerTurn_shouldAllowMultipleBang() async throws {
         // Given
-        let state = GameState.makeBuilderWithCards()
+        let state = GameState.makeBuilderWithAllCards()
             .withPlayer("p1") {
                 $0.withHand([.bang])
-                    .withAttributes([.weapon: 1, .bangsPerTurn: 0])
+                    .withWeapon(1)
             }
             .withPlayer("p2")
             .withPlayedThisTurn([.bang: 1])
             .build()
 
         // When
-        let action = GameAction.preparePlay(.bang, player: "p1")
-        let result = try awaitAction(action, state: state, choose: ["p2"])
+        let action = GameAction.play(.bang, player: "p1")
+        let choices: [Choice] = [
+            .init(options: ["p2"], selectionIndex: 0)
+        ]
+        let result = try await dispatchUntilCompleted(action, state: state, expectedChoices: choices)
 
-        // Assert
-        XCTAssertEqual(result, [
-            .playBrown(.bang, player: "p1"),
-            .chooseOne(.target, options: ["p2"], player: "p1"),
+        // Then
+        #expect(result == [
+            .play(.bang, player: "p1"),
+            .choose("p2", player: "p1"),
+            .shoot("p2", player: "p1"),
             .damage(1, player: "p2")
         ])
     }
-
-    func play_noPlayerReachable_shouldThrowError() async throws {
+    */
+    @Test func play_noPlayerReachable_shouldThrowError() async throws {
         // Given
-        let state = GameState.makeBuilderWithCards()
+        let state = GameState.makeBuilderWithAllCards()
             .withPlayer("p1") {
                 $0.withHand([.bang])
-                    .withAttributes([.bangsPerTurn: 1, .weapon: 1])
+                    .withWeapon(1)
             }
             .withPlayer("p2") {
-                $0.withAttributes([.remoteness: 1])
+                $0.withRemoteness(1)
             }
             .build()
 
         // When
         // Then
-        let action = GameAction.preparePlay(.bang, player: "p1")
-        XCTAssertThrowsError(try awaitAction(action, state: state)) { error in
-            XCTAssertEqual(error as? ArgPlayer.Error, .noPlayer(.selectReachable))
+        let action = GameAction.play(.bang, player: "p1")
+        await #expect(throws: GameError.noChoosableTarget([.reachable])) {
+            try await dispatchUntilCompleted(action, state: state)
         }
     }
- */
 }
