6122e04c10b29dcc6eda2206256a0a4bc21ecb81
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-12-24 14:49:30 +0100

    wip

--- WildWestOnline/Core/Bang/Tests/Cards/Collectible/BarrelTest.swift
+++ WildWestOnline/Core/Bang/Tests/Cards/Collectible/BarrelTest.swift
@@ -145,33 +145,28 @@ struct BarrelTest {
             .damage(1, player: "p2")
         ])
     }
-    /*
-    func test_triggeringBarrel_flippedCardIsHearts_holdingMissedCards_shouldNotAskToCounter() throws {
+
+    @Test func triggeringBarrel_flippedCardIsHearts_holdingMissedCards_shouldNotAskToCounter() async throws {
         // Given
-        let state = GameState.makeBuilderWithCards()
-            .withPlayer("p1") {
-                $0.withHand([.bang])
-                    .withAttributes([.weapon: 1, .missesRequiredForBang: 1, .bangsPerTurn: 1])
-            }
+        let state = GameState.makeBuilderWithAllCards()
+            .withPlayer("p1")
             .withPlayer("p2") {
                 $0.withHand([.missed])
                     .withInPlay([.barrel])
-                    .withAbilities([.playCounterCardsOnShot])
-                    .withAttributes([.flippedCards: 1])
+                    .withDrawCards(1)
             }
             .withDeck(["c1-2♥️"])
             .build()
 
         // When
-        let action = GameAction.preparePlay(.bang, player: "p1")
-        let result = try awaitAction(action, state: state, choose: ["p2"])
+        let action = GameAction.shoot("p2", player: "p1")
+        let result = try await dispatchUntilCompleted(action, state: state)
 
         // Then
-        XCTAssertEqual(result, [
-            .playBrown(.bang, player: "p1"),
-            .chooseOne(.target, options: ["p2"], player: "p1"),
-            .draw
+        #expect(result == [
+            .shoot("p2", player: "p1"),
+            .draw(player: "p2"),
+            .counterShoot(player: "p2")
         ])
     }
- */
 }
