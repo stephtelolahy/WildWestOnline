92bf881691372177a45c865ddfb1aa858da804f7
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-12-27 23:47:55 +0100

    wip

--- WildWestOnline/Core/Bang/Sources/Middleware/AIMiddleware.swift
+++ WildWestOnline/Core/Bang/Sources/Middleware/AIMiddleware.swift
@@ -9,21 +9,67 @@ public extension Middlewares {
     static var playAIMove: Middleware<GameState> {
         { state, _ in
             if let pendingChoice = state.pendingChoice,
-               state.playMode[pendingChoice.chooser] == .auto,
-               let selection = pendingChoice.options.randomElement() {
+               state.playMode[pendingChoice.chooser] == .auto {
+                let actions = pendingChoice.options.map { GameAction.choose($0.label, player: pendingChoice.chooser) }
+                let strategy: AIStrategy = AgressiveStrategy()
+                let bestMove = strategy.evaluateBestMove(actions, state: state)
                 try? await Task.sleep(nanoseconds: state.visibleActionDelayMilliSeconds * 1_000_000)
-                return GameAction.choose(selection.label, player: pendingChoice.chooser)
+                return bestMove
             }
 
             if state.active.isNotEmpty,
                let choice = state.active.first,
-               state.playMode[choice.key] == .auto,
-               let selection = choice.value.randomElement() {
+               state.playMode[choice.key] == .auto {
+                let actions = choice.value.map { GameAction.play($0, player: choice.key) }
+                let strategy: AIStrategy = AgressiveStrategy()
+                let bestMove = strategy.evaluateBestMove(actions, state: state)
                 try? await Task.sleep(nanoseconds: state.visibleActionDelayMilliSeconds * 1_000_000)
-                return GameAction.play(selection, player: choice.key)
+                return bestMove
             }
 
             return nil
         }
+    }
+}
+
+private protocol AIStrategy {
+    func evaluateBestMove(_ actions: [GameAction], state: GameState) -> GameAction
+}
+
+struct RandomStrategy: AIStrategy {
+    func evaluateBestMove(_ actions: [GameAction], state: GameState) -> GameAction {
+        actions.randomElement()!
+    }
+}
+
+struct AgressiveStrategy: AIStrategy {
+    func evaluateBestMove(_ actions: [GameAction], state: GameState) -> GameAction {
+        // swiftlint:disable no_magic_numbers
+        let itemValue: [String: Int] = [
+            "bang": 3,
+            "duel": 3,
+            "gatling": 3,
+            "panic": 1,
+            "catBalou": 1,
+            "endTurn": -1,
+            "pass": -1
+        ]
+
+        return actions.shuffled().min { action1, action2 in
+            let value1 = itemValue[action1.selectedItem] ?? 0
+            let value2 = itemValue[action2.selectedItem] ?? 0
+            return value1 > value2
+        }!
+    }
+}
+
+private extension GameAction {
+    var selectedItem: String {
+        guard let item = payload.card ?? payload.selection else {
+            fatalError("Missing played card or choosen item")
+        }
+
+        // TODO: handle non-card selection
+        return Card.extractName(from: item)
     }
 }
