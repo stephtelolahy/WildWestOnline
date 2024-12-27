b8501eb002eaf2c466ba2302c8f793329d5017e3
Stephano Telolahy <stephano.telolahy@gmail.com>
2024-12-27 23:15:55 +0100

    wip

---
+++ WildWestOnline/Core/Bang/Sources/Game/Ai/AIMiddleware.swift
@@ -0,0 +1,29 @@
+//
+//  AIMiddleware.swift
+//  WildWestOnline
+//
+//  Created by Hugues Telolahy on 27/12/2024.
+//
+
+public extension Middlewares {
+    static var playAIMove: Middleware<GameState> {
+        { state, _ in
+            if let pendingChoice = state.pendingChoice,
+               state.playMode[pendingChoice.chooser] == .auto,
+               let selection = pendingChoice.options.randomElement() {
+                try? await Task.sleep(nanoseconds: state.visibleActionDelayMilliSeconds * 1_000_000)
+                return GameAction.choose(selection.label, player: pendingChoice.chooser)
+            }
+
+            if state.active.isNotEmpty,
+               let choice = state.active.first,
+               state.playMode[choice.key] == .auto,
+               let selection = choice.value.randomElement() {
+                try? await Task.sleep(nanoseconds: state.visibleActionDelayMilliSeconds * 1_000_000)
+                return GameAction.play(selection, player: choice.key)
+            }
+
+            return nil
+        }
+    }
+}
--- WildWestOnline/Core/Bang/Sources/Game/GameMiddleware.swift
+++ WildWestOnline/Core/Bang/Sources/Game/GameMiddleware.swift
@@ -44,27 +44,6 @@ public extension Middlewares {
             return nil
         }
     }
-
-    static var playAIMove: Middleware<GameState> {
-        { state, _ in
-            if let pendingChoice = state.pendingChoice,
-               state.playMode[pendingChoice.chooser] == .auto,
-               let selection = pendingChoice.options.randomElement() {
-                try? await Task.sleep(nanoseconds: state.visibleActionDelayMilliSeconds * 1_000_000)
-                return GameAction.choose(selection.label, player: pendingChoice.chooser)
-            }
-
-            if state.active.isNotEmpty,
-               let choice = state.active.first,
-               state.playMode[choice.key] == .auto,
-               let selection = choice.value.randomElement() {
-                try? await Task.sleep(nanoseconds: state.visibleActionDelayMilliSeconds * 1_000_000)
-                return GameAction.play(selection, player: choice.key)
-            }
-
-            return nil
-        }
-    }
 }
 
 private extension GameState {
