0d557aad550e79b83788c092acdb9ee399aaa896
Stephano Telolahy <stephano.telolahy@gmail.com>
2025-01-02 16:44:10 +0100

    wip

--- WildWestOnline/Utilities/Redux/Tests/StoreTest.swift
+++ WildWestOnline/Utilities/Redux/Tests/StoreTest.swift
@@ -9,85 +9,10 @@ import Redux
 import Testing
 
 struct StoreTest {
-
-    struct AppState: Equatable {
-        var searchResult: [String] = []
-    }
-
-    enum AppAction {
-        case setSearchResults(repos: [String])
-        case search(query: String)
-        case fetchRecent
-    }
-
-    struct AppDependencies {
-        var search: (String) async throws -> [String]
-        var fetchRecent: () async throws -> [String]
-    }
-
-    func appReducer(
-        state: inout AppState,
-        action: AppAction,
-        dependencies: AppDependencies
-    ) -> Effect<AppAction> {
-        switch action {
-        case let .setSearchResults(repos):
-            state.searchResult = repos
-            return .none
-
-        case let .search(query):
-            return .run {
-                do {
-                    let result = try await dependencies.search(query)
-                    return .setSearchResults(repos: result)
-                } catch {
-                    return .setSearchResults(repos: [])
-                }
-
-            }
-
-        case .fetchRecent:
-            return .run {
-                do {
-                    let result = try await dependencies.fetchRecent()
-                    return .setSearchResults(repos: result)
-
-                } catch {
-                    return .setSearchResults(repos: [])
-                }
-            }
-        }
-    }
-
-    struct SearchService {
-        var searchResult: Result<[String], Error> = .success([])
-        var fetchRecentResult: Result<[String], Error> = .success([])
-
-        func search(query: String) async throws -> [String] {
-            switch searchResult {
-            case .success(let data):
-                return data
-            case .failure(let error):
-                throw error
-            }
-        }
-
-        func fetchRecent() async throws -> [String] {
-            switch searchResult {
-            case .success(let data):
-                return data
-            case .failure(let error):
-                throw error
-            }
-        }
-    }
-
-    typealias AppStore = Store<AppState, AppAction, AppDependencies>
-
     @Test func dispatchActionShouldEmitNewState() async throws {
         // Given
         var service = SearchService()
-        service.searchResult = .success(["recent"])
+        service.fetchRecentResult = .success(["recent"])
         let store = await AppStore(
             initialState: .init(),
             reducer: appReducer,
@@ -104,3 +29,77 @@ struct StoreTest {
         await #expect(store.state.searchResult == ["recent"])
     }
 }
+
+struct AppState: Equatable {
+    var searchResult: [String] = []
+}
+
+enum AppAction {
+    case setSearchResults(repos: [String])
+    case search(query: String)
+    case fetchRecent
+}
+
+struct AppDependencies {
+    var search: (String) async throws -> [String]
+    var fetchRecent: () async throws -> [String]
+}
+
+func appReducer(
+    state: inout AppState,
+    action: AppAction,
+    dependencies: AppDependencies
+) -> Effect<AppAction> {
+    switch action {
+    case let .setSearchResults(repos):
+        state.searchResult = repos
+        return .none
+
+    case let .search(query):
+        return .run {
+            do {
+                let result = try await dependencies.search(query)
+                return .setSearchResults(repos: result)
+            } catch {
+                return .setSearchResults(repos: [])
+            }
+
+        }
+
+    case .fetchRecent:
+        return .run {
+            do {
+                let result = try await dependencies.fetchRecent()
+                return .setSearchResults(repos: result)
+
+            } catch {
+                return .setSearchResults(repos: [])
+            }
+        }
+    }
+}
+
+struct SearchService {
+    var searchResult: Result<[String], Error> = .success([])
+    var fetchRecentResult: Result<[String], Error> = .success([])
+
+    func search(query: String) async throws -> [String] {
+        switch searchResult {
+        case .success(let data):
+            return data
+        case .failure(let error):
+            throw error
+        }
+    }
+
+    func fetchRecent() async throws -> [String] {
+        switch fetchRecentResult {
+        case .success(let data):
+            return data
+        case .failure(let error):
+            throw error
+        }
+    }
+}
+
+typealias AppStore = Store<AppState, AppAction, AppDependencies>
