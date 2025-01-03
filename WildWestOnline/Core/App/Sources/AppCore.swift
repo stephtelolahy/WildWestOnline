23d7db6c9e82aeea4bb0053d6b79236bccd46385
Stephano Telolahy <stephano.telolahy@gmail.com>
2025-01-03 02:08:16 +0100

    wip

---
+++ WildWestOnline/Core/App/Sources/AppCore.swift
@@ -0,0 +1,60 @@
+//
+//  AppCore.swift
+//  WildWestOnline
+//
+//  Created by Hugues Stéphano TELOLAHY on 03/01/2025.
+//
+
+import Redux
+import NavigationCore
+import SettingsCore
+import GameCore
+
+/// Global app state
+/// Organize State Structure Based on Data Types, Not Components
+/// https://redux.js.org/style-guide/#organize-state-structure-based-on-data-types-not-components
+public struct AppState: Codable, Equatable {
+    public var navigation: NavigationState
+    public var settings: SettingsState
+    public let inventory: Inventory
+    public var game: GameState?
+
+    public init(
+        navigation: NavigationState,
+        settings: SettingsState,
+        inventory: Inventory,
+        game: GameState? = nil
+    ) {
+        self.navigation = navigation
+        self.settings = settings
+        self.inventory = inventory
+        self.game = game
+    }
+}
+
+public enum AppAction {
+    case navigation(NavigationAction)
+    case settings(SettingsAction)
+    case game(GameAction)
+    case setup(GameSetupAction)
+}
+
+public func appReducer(
+    state: inout AppState,
+    action: AppAction,
+    dependencies: Void
+) throws -> Effect<AppAction> {
+    switch action {
+    case .navigation(let navigationAction):
+        return try navigationReducer(state: &state.navigation, action: navigationAction, dependencies: ())
+
+    case .settings(let settingsAction):
+        return try settingsReducer(state: &state.settings, action: settingsAction, dependencies: ())
+
+    case .game(let gameAction):
+        return try gameReducer(state: &state.game, action: gameAction, dependencies: ())
+
+    case .setup(let setupAction):
+        return try gameSetupReducer(state: &state, action: setupAction, dependencies: ())
+    }
+}
---
+++ WildWestOnline/Core/App/Sources/GameSetupCore.swift
@@ -0,0 +1,92 @@
+//
+//  GameSetupCore.swift
+//  WildWestOnline
+//
+//  Created by Hugues Stéphano TELOLAHY on 03/01/2025.
+//
+
+import Redux
+import GameCore
+import SettingsCore
+import NavigationCore
+
+public enum GameSetupAction: Sendable {
+    case startGame
+    case quitGame
+    case setGame(GameState)
+    case unsetGame
+}
+
+func gameSetupReducer(
+    state: inout AppState,
+    action: GameSetupAction,
+    dependencies: Void
+) throws -> Effect<GameSetupAction> {
+    switch action {
+    case .startGame:
+        return .none
+        
+    case .quitGame:
+        return .none
+        
+    case .setGame(let game):
+        state.game = game
+
+    case .unsetGame:
+        state.game = nil
+    }
+
+    return .none
+}
+
+/*
+
+ public extension Middlewares {
+     static var setupGame: Middleware<AppState> {
+         { state, action in
+             guard let action = action as? GameSetupAction else {
+                 return nil
+             }
+
+             return switch action {
+             case .startGame:
+                 GameSetupAction.setGame(
+                         AppState.createGame(
+                             settings: state.settings,
+                             inventory: state.inventory
+                         )
+                     )
+
+             case .setGame:
+                 NavigationStackAction<MainDestination>.push(.game)
+
+             case .quitGame:
+                 GameSetupAction.unsetGame
+
+             case .unsetGame:
+                 NavigationStackAction<MainDestination>.pop
+             }
+         }
+     }
+ }
+
+ private extension AppState {
+     static func createGame(settings: SettingsState, inventory: Inventory) -> GameState {
+         var game = Setup.buildGame(
+             playersCount: settings.playersCount,
+             inventory: inventory,
+             preferredFigure: settings.preferredFigure
+         )
+
+         let manualPlayer: String? = settings.simulation ? nil : game.playOrder[0]
+         game.playMode = game.playOrder.reduce(into: [:]) {
+             $0[$1] = $1 == manualPlayer ? .manual : .auto
+         }
+
+         game.actionDelayMilliSeconds = settings.actionDelayMilliSeconds
+
+         return game
+     }
+ }
+
+ */
