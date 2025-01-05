40b27d5cf5d4738103b15c8d3d923eab771c58e8
Stephano Telolahy <stephano.telolahy@gmail.com>
2025-01-05 16:16:09 +0100

    wip

--- WildWestOnline/Core/App/Sources/SetupGameCore.swift
+++ WildWestOnline/Core/App/Sources/SetupGameCore.swift
@@ -28,13 +28,33 @@ func setupGameReducer(
 
     switch action {
     case .startGame:
-        return .none
+        let state = state
+        return .group([
+            .run {
+                SetupGameAction.setGame(
+                    AppState.createGame(
+                        settings: state.settings,
+                        inventory: state.inventory
+                    )
+                )
+            },
+            .run {
+                NavigationStackAction<MainDestination>.push(.game)
+            }
+        ])
 
     case .setGame(let game):
         state.game = game
 
     case .quitGame:
-        return .none
+        return .group([
+            .run {
+                SetupGameAction.unsetGame
+            },
+            .run {
+                NavigationStackAction<MainDestination>.pop
+            }
+        ])
 
     case .unsetGame:
         state.game = nil
@@ -43,54 +63,21 @@ func setupGameReducer(
     return .none
 }
 
-/*
+private extension AppState {
+    static func createGame(settings: SettingsState, inventory: Inventory) -> GameState {
+        var game = Setup.buildGame(
+            playersCount: settings.playersCount,
+            inventory: inventory,
+            preferredFigure: settings.preferredFigure
+        )
 
- public extension Middlewares {
-     static var setupGame: Middleware<AppState> {
-         { state, action in
-             guard let action = action as? GameSetupAction else {
-                 return nil
-             }
+        let manualPlayer: String? = settings.simulation ? nil : game.playOrder[0]
+        game.playMode = game.playOrder.reduce(into: [:]) {
+            $0[$1] = $1 == manualPlayer ? .manual : .auto
+        }
 
-             return switch action {
-             case .startGame:
-                 GameSetupAction.setGame(
-                         AppState.createGame(
-                             settings: state.settings,
-                             inventory: state.inventory
-                         )
-                     )
+        game.actionDelayMilliSeconds = settings.actionDelayMilliSeconds
 
-             case .setGame:
-                 NavigationStackAction<MainDestination>.push(.game)
-
-             case .quitGame:
-                 GameSetupAction.unsetGame
-
-             case .unsetGame:
-                 NavigationStackAction<MainDestination>.pop
-             }
-         }
-     }
- }
-
- private extension AppState {
-     static func createGame(settings: SettingsState, inventory: Inventory) -> GameState {
-         var game = Setup.buildGame(
-             playersCount: settings.playersCount,
-             inventory: inventory,
-             preferredFigure: settings.preferredFigure
-         )
-
-         let manualPlayer: String? = settings.simulation ? nil : game.playOrder[0]
-         game.playMode = game.playOrder.reduce(into: [:]) {
-             $0[$1] = $1 == manualPlayer ? .manual : .auto
-         }
-
-         game.actionDelayMilliSeconds = settings.actionDelayMilliSeconds
-
-         return game
-     }
- }
-
- */
+        return game
+    }
+}
