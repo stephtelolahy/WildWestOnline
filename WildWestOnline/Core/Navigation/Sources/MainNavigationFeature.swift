450ce1f852a222d6fd278ab4e40576e5e937fbc4
Stephano Telolahy <stephano.telolahy@gmail.com>
2025-03-29 22:01:30 +0100

    wip

---
+++ WildWestOnline/Core/Navigation/Sources/MainNavigationFeature.swift
@@ -0,0 +1,65 @@
+//
+//  MainNavigationFeature.swift
+//  WildWestOnline
+//
+//  Created by Hugues Stéphano TELOLAHY on 29/03/2025.
+//
+
+import Redux
+
+public enum MainNavigationFeature {
+    public struct State: Equatable, Codable, Sendable {
+        public var path: [Destination]
+        public var settingsSheet: SettingsNavigationFeature.State?
+
+        public enum Destination: String, Codable, Sendable {
+            case home
+            case game
+        }
+
+        public init(
+            path: [Destination],
+            settingsSheet: SettingsNavigationFeature.State? = nil
+        ) {
+            self.path = path
+            self.settingsSheet = settingsSheet
+        }
+    }
+
+    public enum Action: ActionProtocol {
+        case push(State.Destination)
+        case pop
+        case setPath([State.Destination])
+        case presentSettingsSheet
+        case dismissSettingsSheet
+    }
+
+    public static func reduce(
+        into state: inout State,
+        action: ActionProtocol,
+        dependencies: Void
+    ) throws -> Effect {
+        guard let action = action as? Action else {
+            return .none
+        }
+
+        switch action {
+        case .push(let page):
+            state.path.append(page)
+
+        case .pop:
+            state.path.removeLast()
+
+        case .setPath(let path):
+            state.path = path
+
+        case .presentSettingsSheet:
+            state.settingsSheet = .init(path: [])
+
+        case .dismissSettingsSheet:
+            state.settingsSheet = nil
+        }
+
+        return .none
+    }
+}
---
+++ WildWestOnline/Core/Navigation/Sources/SettingsNavigationFeature.swift
@@ -0,0 +1,47 @@
+//
+//  SettingsNavigationFeature.swift
+//  WildWestOnline
+//
+//  Created by Hugues Stéphano TELOLAHY on 29/03/2025.
+//
+
+import Redux
+
+public enum SettingsNavigationFeature {
+    public struct State: Equatable, Codable, Sendable {
+        public var path: [Destination]
+
+        public enum Destination: String, Codable, Sendable {
+            case figures
+        }
+    }
+
+    public enum Action: ActionProtocol {
+        case push(State.Destination)
+        case pop
+        case setPath([State.Destination])
+    }
+
+    public static func reduce(
+        into state: inout State,
+        action: ActionProtocol,
+        dependencies: Void
+    ) throws -> Effect {
+        guard let action = action as? Action else {
+            return .none
+        }
+
+        switch action {
+        case .push(let page):
+            state.path.append(page)
+
+        case .pop:
+            state.path.removeLast()
+
+        case .setPath(let path):
+            state.path = path
+        }
+
+        return .none
+    }
+}
