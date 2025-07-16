7f14f82548e5f58f4e461708eac5e53adbca6d70
Stephano Telolahy <stephano.telolahy@gmail.com>
2025-07-16 09:06:29 +0200

    refactor

--- WildWestOnline/GameCore/Sources/GameFeature.swift
+++ WildWestOnline/GameCore/Sources/GameFeature.swift
@@ -67,3 +67,17 @@ public enum GameFeature {
         ])
     }
 }
+
+public extension GameFeature.State {
+    var pendingChoice: Card.Selector.ChooseOneResolved? {
+        guard let nextAction = queue.first,
+              let selector = nextAction.selectors.first,
+              case let .chooseOne(_, resolved, selection) = selector,
+              let choice = resolved,
+              selection == nil else {
+            return nil
+        }
+
+        return choice
+    }
+}
