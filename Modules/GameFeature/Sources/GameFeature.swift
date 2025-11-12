9cd9612aff3a5a6c37184c61ee5e13ec51f68797
stephtelolahy <stephano.telolahy@gmail.com>
2025-11-12 23:18:07 +0100

    Update GameFeature.swift

--- Modules/GameFeature/Sources/GameFeature.swift
+++ Modules/GameFeature/Sources/GameFeature.swift
@@ -23,6 +23,7 @@ public enum GameFeature {
         public var lastError: GameFeature.Error?
         public var turn: String?
         public var playedThisTurn: [String: Int]
+        #warning("this attribute is used by specific card effect")
         public var cardsToDrawThisTurn: Int
         public var active: [String: [String]]
         public var isOver: Bool
