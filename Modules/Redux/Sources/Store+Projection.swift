623975ef48447e569906c060e23f64ed142e609c
Stephano Telolahy <stephano.telolahy@gmail.com>
2025-11-25 22:25:38 +0100

    wip

--- Modules/Redux/Sources/Store+Projection.swift
+++ Modules/Redux/Sources/Store+Projection.swift
@@ -10,56 +10,30 @@
 /// It won't store anything, only project the original store.
 public extension Store {
     func projection<LocalState: Equatable, LocalAction>(
-        state toLocalState: @escaping (State) -> LocalState?,
+        state deriveState: @escaping (State) -> LocalState?,
         action embedAction: @escaping (LocalAction) -> Action
     ) -> Store<LocalState, LocalAction, Void> {
-        // TODO remove StoreProjection
-        StoreProjection(
-            globalStore: self,
-            deriveState: toLocalState,
-            embedAction: embedAction
-        )
-    }
-}
-
-private final class StoreProjection<
-    LocalState: Equatable,
-    LocalAction,
-    GlobalState,
-    GlobalAction,
-    Dependencies
->: Store<LocalState, LocalAction, Void> {
-    private let globalStore: Store<GlobalState, GlobalAction, Dependencies>
-    private let toLocalState: (GlobalState) -> LocalState?
-    private let embedAction: (LocalAction) -> GlobalAction
-
-    init(
-        globalStore: Store<GlobalState, GlobalAction, Dependencies>,
-        deriveState: @escaping (GlobalState) -> LocalState?,
-        embedAction: @escaping (LocalAction) -> GlobalAction
-    ) {
-        guard let initialState = deriveState(globalStore.state) else {
-            fatalError("Failed deriving \(LocalState.self) from \(GlobalState.self): \(globalStore.state)")
+        guard let initialState = deriveState(state) else {
+            fatalError("Failed deriving \(LocalState.self) from \(State.self): \(state)")
         }
 
-        self.globalStore = globalStore
-        self.toLocalState = deriveState
-        self.embedAction = embedAction
-
-        super.init(
+        let store = Store<LocalState, LocalAction, Void>(
             initialState: initialState,
-            reducer: { _, _, _ in .none },
+            reducer: { _, action, _ in
+                .run {
+                    await self.dispatch(embedAction(action))
+                    return .none
+                }
+            },
             dependencies: ()
         )
 
-        globalStore.$state
+        $state
             .map(deriveState)
             .compactMap { $0 }
             .removeDuplicates()
-            .assign(to: &self.$state)
-    }
+            .assign(to: &store.$state)
 
-    override func dispatch(_ action: LocalAction) async {
-        await globalStore.dispatch(embedAction(action))
+        return store
     }
 }
