2efe848b3cc42c574ff6cef6554a04ad6dbfaceb
Stephano Telolahy <stephano.telolahy@gmail.com>
2025-11-26 11:12:53 +0100

    wip

--- Modules/CardResources/Sources/Modifiers/IgnoreLimitPerTurn.swift
+++ Modules/CardResources/Sources/Modifiers/IgnoreLimitPerTurn.swift
@@ -15,6 +15,27 @@ struct IgnoreLimitPerTurn: QueueModifierHandler {
     static let id = GameFeature.Action.QueueModifier.ignoreLimitPerTurn
 
     static func apply(_ action: GameFeature.Action, state: GameFeature.State) throws(GameFeature.Error) -> [GameFeature.Action] {
-        fatalError("Unimplemented")
+        guard let playIndex = state.queue.firstIndex(where: {
+            $0.name == .play
+        }) else {
+            fatalError("Missing play action")
+        }
+
+        var playAction = state.queue[playIndex]
+        guard let limitPerTurnIndex = playAction.selectors.firstIndex(where: {
+            if case let .require(requirement) = $0,
+               case .playLimitThisTurn = requirement {
+                return true
+            } else {
+                return false
+            }
+        }) else {
+            fatalError("Missing play limit per turn requirement")
+        }
+
+        playAction.selectors.remove(at: limitPerTurnIndex)
+        var queue = state.queue
+        queue[playIndex] = playAction
+        return queue
     }
 }
