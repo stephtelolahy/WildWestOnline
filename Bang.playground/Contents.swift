1cd351c719672e48f46205c22f780f2774af27be
Stéphano Telolahy <stephano.telolahy@gmail.com>
2026-05-15 18:58:58 +0200

    wip

--- Bang.playground/Contents.swift
+++ Bang.playground/Contents.swift
@@ -16,6 +16,26 @@ struct Card {
     var onShootingWithBangCard: Effect?
 }
 
+struct CardEffect {
+    let trigger: GameEvent
+    let action: Effect
+}
+
+typealias CardDefinition = [CardEffect]
+
+enum GameEvent {
+    case cardPlayed
+    case cardEquipped
+    case playerShot
+    case turnStarted
+
+    case playerDamaged(target: PlayerID, source: PlayerID)
+    case playerEliminated(target: PlayerID, source: PlayerID)
+}
+
+enum PlayerID {}
+enum CardID {}
+
 indirect enum Effect {
     // MARK: - Logic
     case `repeat`(Repeat, Self)
@@ -25,6 +45,7 @@ indirect enum Effect {
     case askForCounter(String, Self)
     case askForRedirect(String, Self)
     case askForCost(Int, Self)
+    case require(Requirement, Self)
 
     // MARK: - Action
     case drawDeck(PlayerRef)
@@ -51,6 +72,7 @@ indirect enum Effect {
     case setCardsPerTurn(Int)
     case incrementRequiredMisses
     case playAs(String, String)
+    case nothing
 }
 
 enum PlayerRef {
@@ -61,6 +83,7 @@ enum PlayerRef {
     case chooseAny
     case chooseAnyAtWeaponRange
     case chooseAnyWithCard
+    case chooseAnyWithCardAtRange(Int)
     case chooseAnyWithHandCard
 }
 
@@ -89,107 +112,183 @@ enum Requirement {
 
 // MARK: - Cards
 
-extension Card {
-    static let stagecoach = Card(
-        onPlay: .repeat(.times(2), .drawDeck(.me))
-    )
-
-    static let wellsFargo = Card(
-        onPlay: .repeat(.times(3), .drawDeck(.me))
-    )
-
-    static let beer = Card(
-        onPlay: .heal(1, .me),
-        canPlay: .playersAtLeast(3)
-    )
-
-    static let saloon = Card(
-        onPlay: .repeat(.perPlayerWounded, .heal(1, .i))
-    )
-
-    static let catBalou = Card(
-        onPlay: .discard(.chooseAnyWithCard, .chooseAnyTargetCard)
-    )
-
-    static let panic = Card(
-        onPlay: .steal(.chooseAnyWithCard, .chooseAnyTargetCard, .me)
-    )
-
-    static let generalStore = Card(
-        onPlay: .concat([
-            .repeat(.perPlayerActive, .discover),
-            .repeat(.perPlayerActive, .drawDiscovered(.chooseAnyDiscoveredCard, .i))
-        ])
-    )
-
-    static let bang = Card(
-        onPlay: .shoot(.chooseAnyAtWeaponRange),
-        canPlay: .playLimit(1)
-    )
-
-    static let missed = Card(
-        onPlay: .dodge(.me)
-    )
-
-    static let gatling = Card(
-        onPlay: .repeat(.perPlayerOther, .shoot(.i))
-    )
-
-    static let indians = Card(
-        onPlay: .repeat(.perPlayerOther, .askForCounter("bang", .damage(1, .i)))
-    )
-
-    static let duel = Card(
-        onPlay: .askForRedirect("bang", .damage(1, .chooseAny))
-    )
-
-    static let schofield = Card(
-        onActive: .setWeaponRange(2)
-    )
-
-    static let remington = Card(
-        onActive: .setWeaponRange(3)
-    )
-
-    static let revCarabine = Card(
-        onActive: .setWeaponRange(4)
-    )
-
-    static let winchester = Card(
-        onActive: .setWeaponRange(5)
-    )
-
-    static let volcanic = Card(
-        onActive: .concat([
-            .setWeaponRange(1),
-            .ignorePlayLimit("bang")
-        ])
-    )
-
-    static let scope = Card(
-        onActive: .incrementMagnifying
-    )
-
-    static let mustang = Card(
-        onActive: .incrementRemoteness
-    )
-
-    static let barrel = Card(
-        onShot: .draw("♥️", then: .dodge(.me))
-    )
-
-    static let dynamite = Card(
-        onTurnStarted: .draw(
-            "([2|3|4|5|6|7|8|9]♠️)",
-            then: .concat([
-                .damage(3, .me),
-                .discard(.me, .sourceCard)
-            ]) ,
-            else: .passLeft(.sourceCard)
+extension CardDefinition {
+    static var stagecoach: Self {
+        [
+            .init(
+                trigger: .cardPlayed,
+                action: .repeat(.times(2), .drawDeck(.me))
+            )
+        ]
+    }
+/*
+    static var wellsFargo: Self {
+        .init(
+            trigger: .cardPlayed,
+            action: .repeat(.times(3), .drawDeck(.me))
         )
-    )
+    }
 
-    static let jail = Card(
+    static var beer: Self {
+        .init(
+            trigger: .cardPlayed,
+            action: .require(.playersAtLeast(3), .heal(1, .me))
+        )
+    }
+
+    static var saloon: Self {
+        .init(
+            trigger: .cardPlayed,
+            action: .repeat(.perPlayerWounded, .heal(1, .i))
+        )
+    }
+
+    static var catBalou: Self {
+        .init(
+            trigger: .cardPlayed,
+            action: .discard(.chooseAnyWithCard, .chooseAnyTargetCard)
+        )
+    }
+
+    static var panic: Self {
+        .init(
+            trigger: .cardPlayed,
+            action: .steal(.chooseAnyWithCardAtRange(1), .chooseAnyTargetCard, .me)
+        )
+    }
+
+    static var generalStore: Self {
+        .init(
+            trigger: .cardPlayed,
+            action: .concat([
+                .repeat(.perPlayerActive, .discover),
+                .repeat(.perPlayerActive, .drawDiscovered(.chooseAnyDiscoveredCard, .i))
+            ])
+        )
+    }
+
+    static var bang: Self {
+        .init(
+            trigger: .cardPlayed,
+            action: .require(.playLimit(1), .shoot(.chooseAnyAtWeaponRange))
+        )
+    }
+
+    static var missed: Self {
+        .init(
+            trigger: .cardPlayed,
+            action: .dodge(.me)
+        )
+    }
+
+    static var gatling: Self {
+        .init(
+            trigger: .cardPlayed,
+            action: .repeat(.perPlayerOther, .shoot(.i))
+        )
+    }
+
+    static var indians: Self {
+        .init(
+            trigger: .cardPlayed,
+            action: .repeat(.perPlayerOther, .askForCounter("bang", .damage(1, .i)))
+        )
+    }
+
+    static var duel: Self {
+        .init(
+            trigger: .cardPlayed,
+            action: .askForRedirect("bang", .damage(1, .chooseAny))
+        )
+    }
+
+    static var schofield: Self {
+        .init(
+            trigger: .cardEquipped,
+            action: .setWeaponRange(2)
+        )
+    }
+
+    static var remington: Self {
+        .init(
+            trigger: .cardEquipped,
+            action: .setWeaponRange(3)
+        )
+    }
+
+    static var revCarabine: Self {
+        .init(
+            trigger: .cardEquipped,
+            action: .setWeaponRange(4)
+        )
+    }
+
+    static var winchester: Self {
+        .init(
+            trigger: .cardEquipped,
+            action: .setWeaponRange(5)
+        )
+    }
+
+    static var volcanic: Self {
+        .init(
+            trigger: .cardEquipped,
+            action: .concat([
+                .setWeaponRange(1),
+                .ignorePlayLimit("bang")
+            ])
+        )
+    }
+
+    static var scope: Self {
+        .init(
+            trigger: .cardEquipped,
+            action: .incrementMagnifying
+        )
+    }
+
+    static var mustang: Self {
+        .init(
+            trigger: .cardEquipped,
+            action: .incrementRemoteness
+        )
+    }
+*/
+    static var barrel: Self {
+        [
+            .init(
+                trigger: .cardEquipped,
+                action: .nothing
+            ),
+            .init(
+                trigger: .playerShot,
+                action: .draw("♥️", then: .dodge(.me))
+            )
+        ]
+    }
+
+    static var dynamite: Self {
+        [
+            .init(
+                trigger: .cardEquipped,
+                action: .nothing
+            ),
+            .init(
+                trigger: .turnStarted,
+                action: .draw(
+                    "([2|3|4|5|6|7|8|9]♠️)",
+                    then: .concat([
+                        .damage(3, .me),
+                        .discard(.me, .sourceCard)
+                    ]) ,
+                    else: .passLeft(.sourceCard)
+                )
+            )
+        ]
+    }
+
+        /*
+    static var jail: Self {
         onTurnStarted: .concat([
             .draw(
                 "not(♥️)",
@@ -199,68 +298,68 @@ extension Card {
         ])
     )
 
-    static let willyTheKid = Card(
+    static var willyTheKid: Self {
         onActive: .ignorePlayLimit("bang")
     )
 
-    static let roseDoolan = Card(
+    static var roseDoolan: Self {
         onActive: .incrementMagnifying
     )
 
-    static let paulRegret = Card(
+    static var paulRegret: Self {
         onActive: .incrementRemoteness
     )
 
-    static let bartCassidy = Card(
+    static var bartCassidy: Self {
         onDamaged: .repeat(.perDamage, .drawDeck(.me))
     )
 
-    static let elGringo = Card(
+    static var elGringo: Self {
         onDamaged: .repeat(.perDamage, .steal(.attacker, .chooseAnyTargetCard, .me))
     )
 
-    static let suzyLafayette = Card(
+    static var suzyLafayette: Self {
         onHandEmptied: .drawDeck(.me)
     )
 
-    static let jourdonnais = Card(
+    static var jourdonnais: Self {
         onShot: .draw("♥️", then: .dodge(.me))
     )
 
-    static let sidKetchum = Card(
+    static var sidKetchum: Self {
         onActive: .askForCost(2, .heal(1, .me))
     )
 
-    static let vultureSam = Card(
+    static var vultureSam: Self {
         onOtherEliminated: .repeat(.perCardOfEliminatedPlayer, .steal(.eliminated, .i, .me))
     )
 
-    static let luckyDuke = Card(
+    static var luckyDuke: Self {
         onDrawRequired: .incrementDrawCards
     )
 
-    static let blackJack = Card(
+    static var blackJack: Self {
         onDrawLastCardOnTurnStarted: .concat([
             .showLastHand(.me),
             .draw("(♥️)|(♦️)", then: .drawDeck(.me))
         ])
     )
 
-    static let pedroRamirez = Card(
+    static var pedroRamirez: Self {
         onTurnStarted: .chain([
             .steal(.chooseAnyWithHandCard, .chooseAnyTargetHandCard, .me),
             .setCardsPerTurn(1)
         ])
     )
 
-    static let jesseJones = Card(
+    static var jesseJones: Self {
         onTurnStarted: .chain([
             .drawDiscared(.chooseDiscardedCard, .me),
             .setCardsPerTurn(1)
         ])
     )
 
-    static let kitCarlson = Card(
+    static var kitCarlson: Self {
         onTurnStarted: .chain([
             .repeat(.times(3), .discover),
             .repeat(.times(2), .drawDiscovered(.chooseAnyDiscoveredCard, .me)),
@@ -269,14 +368,15 @@ extension Card {
         ])
     )
 
-    static let slabTheKiller = Card(
+    static var slabTheKiller: Self {
         onShootingWithBangCard: .incrementRequiredMisses
     )
 
-    static let calamityJanet = Card(
+    static var calamityJanet: Self {
         onActive: .concat([
             .playAs("missed", "bang"),
             .playAs("bang", "missed")
         ])
     )
+        */
 }
